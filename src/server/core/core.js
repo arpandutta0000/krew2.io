5dd5d4d79d09408371c3795ae801f96972aeeb5e
Damien Vesper <ldamienvesper@gmail.com>
2020-11-22 12:02:15 -0600

    Add core.

---
+++ src/server/core/core.js
@@ -0,0 +1,20 @@
+let entities = {}
+
+let boats = {}
+let players = {}
+let playerNames = {}
+let sceneCanBalls = {}
+let sceneLines = {}
+let Landmarks = {}
+let pickups = {}
+let bots = {}
+
+// 1000 is the default worldsize.
+let worldsize = 1700;
+
+let iterateEntities = dt => {
+    // Tick each entity.
+    for(let i in entities) {
+        if(entities.hasOwnProperty(e)) entities[e].tick(dt);
+    }
+}
