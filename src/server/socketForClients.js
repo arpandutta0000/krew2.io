baa7b094c85f958d5deb9d5e527bff7eaae5459d
Damien Vesper <ldamienvesper@gmail.com>
2020-12-19 14:06:17 -0600

    fix fountain of youth + logging

--- src/server/socketForClients.js
+++ src/server/socketForClients.js
@@ -1110,7 +1110,7 @@ io.on(`connection`, async socket => {
                 if(item.id == `11` &&  playerEntity.gold >= 1e5) {
                     if(playerEntity.overallCargo >= 1e3 && playerEntity.shipsSank >= 10) {
                         playerEntity.purchaseItem(item.id);
-                        log(`magenta`, `Player ${playerEntity.name} is buying item ${item} (Demolisher) while having ${playerEntity.gold} | IP: ${playerEntity.socket.handshake.address} | Server ${playerEntity.serverNumber}.`);
+                        log(`magenta`, `Player ${playerEntity.name} is buying item`, item, ` (Demolisher) while having ${playerEntity.gold} | IP: ${playerEntity.socket.handshake.address} | Server ${playerEntity.serverNumber}.`);
                     }
                 }
                 else if(item.id == `14` && playerEntity.gold >= 15e4) {
@@ -1121,12 +1121,12 @@ io.on(`connection`, async socket => {
                         playerEntity.availablepoints = playerEntity.level;
                         playerEntity.statsReset = true;
                         playerEntity.purchaseItem(item.id);
-                        log(`magenta`, `Player ${playerEntity.name} is buying item ${item} (Fountain of Youth) while having ${playerENtity.gold} gold | IP: ${playerEntity.socket.handshake.address} | Server ${playerEntity.serverNumber}.`);
+                        log(`magenta`, `Player ${playerEntity.name} is buying item `, item, ` (Fountain of Youth) while having ${playerEntity.gold} gold | IP: ${playerEntity.socket.handshake.address} | Server ${playerEntity.serverNumber}.`);
                     }
                 }
                 else {
                     playerEntity.purchaseItem(item.id);
-                    log(`magenta`, `Player ${playerEntity.name} is buying item ${item} while having ${playerEntity.gold} gold | IP: ${playerEntity.socket.handshake.address} | Server: ${playerEntity.serverNumber}.`);
+                    log(`magenta`, `Player ${playerEntity.name} is buying item `, item, ` while having ${playerEntity.gold} gold | IP: ${playerEntity.socket.handshake.address} | Server: ${playerEntity.serverNumber}.`);
                 }
             }
 
