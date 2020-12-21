8688f66b47162565819bff8973b5fb9ea4b2af37
Damien Vesper <ldamienvesper@gmail.com>
2020-12-21 11:04:26 -0600

    Revert "push es6 game"
    This reverts commit a106df1e45f1207f8cbfa50b764cab2cb5b4dece.

--- src/server/game/game.js
+++ src/server/game/game.js
@@ -1,58 +1,74 @@
-let socket = require(`../socketForClients.js`);
-
+let socket = require('../socketForClients.js');
 let amountChests = 0;
 let respawnChestsDate = undefined;
 
-const log = require(`../utils/log.js`);
+let log = require(`../utils/log.js`);
 log(`green`, `Game is listening at port ${process.env.port}.`);
 
-// Create islands.
-for(landmark of core.config.landmarks) core.createLandmark(landmark.type, landmark.x, landmark.y, landmark);
+// create islands
+for (let i in core.config.landmarks) {
+    let landmark = core.config.landmarks[i];
+    core.createLandmark(landmark.type, landmark.x, landmark.y, landmark);
+}
 
-// Create the main game loop.
+// the main game loop.
 lastFrameTime = Date.now();
-
-// Update at 60 / 10 fps.
-setInterval(() => {
+setInterval(function () {
     let thisFrame = Date.now();
-    let dt = (thisFrame - lastFrameTime) / 1e3;
+    let dt = (thisFrame - lastFrameTime) / 1000;
     lastFrameTime = thisFrame;
 
     core.iterateEntities(dt);
-    socket.send();
-}, 150);
 
-setInterval(() => {
-    // Delete residing impacts, pickups, and projectiles every 15 minutes.
-    for (entity of core.entities) {
+    socket.send();
+
+}, 150); // update at 60/10 fps
+
+/*setInterval(function(){
+  global.gc();
+  console.log('GC done')
+}, 10000);*/
+
+setInterval(function () {
+
+    // Delete residing impacts, pickups, and projectiles
+    for (e in core.entities) {
+        let entity = core.entities[e];
         if (entity.netType == 2 || entity.netType == 3 || entity.netType == 4) {
-            if (entity.netType == 4 && entity.type != 1) core.removeEntity(entity);
+            if (entity.netType == 4 && entity.type != 1)
+                continue;
+
+            core.removeEntity(entity);
         }
     }
-}, 9e5);
 
-// Slower game loop for general event cleanups and leaderboard.
-setInterval(() => {
-    // Push player scores to all players every second.
+}, 900000); // clean the game each 15 minutes
+
+// slower game loop for general event clean-ups and leaderboard
+setInterval(function () {
+
+    // push player scores to all players every second
     let scores = {
         players: [],
-        boats: []
-    }
+        boats: [],
+    };
     let now = new Date();
 
-    for (player of core.players) {
+    for (i in core.players) {
+        let player = core.players[i];
         scores.players.push({
             id: player.id,
             n: player.name,
+            //salary: parseFloat(player.salary).toFixed(0),
             s: parseFloat(player.score).toFixed(0),
             pI: player.parent ? player.parent.id : undefined,
             g: parseFloat(player.gold).toFixed(0),
-            cU: player.cargoused,
+            cU: player.cargoUsed,
             sS: player.shipsSank,
             ok: player.parent ? player.parent.overall_kills : undefined,
             oc: player.parent ? player.parent.overall_cargo : undefined,
             oql: player.parent ? player.parent.other_quest_level : undefined,
-            d: (!player.deaths ? 0 : player.deaths),
+            d: (player.deaths === undefined ? 0 : player.deaths),
             l: player.level,
             c: player.clan,
             cL: player.clanLeader,
@@ -60,80 +76,102 @@ setInterval(() => {
             cR: player.clanRequest
         });
 
-        // If the player has been afk for more than 30 minutes.
-        if ((now - player.lastMoved) > 18e5 && !Admins.includes(player.name) && !Mods.includes(player.name) && Devs.includes(player.name)) {
-            if (player.socket != undefined) {
-                log(`magenta`, `Player ${player.name} was kicked due to AFK timeout | IP: ${player.socket.handshake.address}`);
+        // if the player has been afk for more than 30 minutes
+        if ((now - player.lastMoved) > 1.8e6 && !Mods.includes(player.name)) {
+            if (player.socket !== undefined) {
+                // console.log("Player", player.name, "was kicked due to AFK timeout | IP:", player.socket.handshake.address);
                 player.socket.disconnect();
             }
             core.removeEntity(player);
         }
+
     }
 
-    // Remove crewless boats and add the boat scores.
-    for (let i in core.boats) {
-        let boat = core.boats[i];
-        // Remove any bots / ghost ships (ships without krew).
-        if (boat.krewCount < 1) return core.removeEntity(boat);
-        if ((now - boat.lastMoved) > 6e5) boat.recruiting = false;
-        if (boat.shipState == 4 & boat.departureTime > 0 && (now - boat.lastMoved) > 1e3) {
+    // remove crewless boats and add the boat scores
+    for (j in core.boats) {
+        let boat = core.boats[j];
+
+        //boat.updateProps();
+
+        // this will remove bots for some reason
+        if (boat.krewCount <= 0) {
+            core.removeEntity(boat);
+            continue;
+        }
+
+        if ((now - boat.lastMoved) > 600000) {
+            boat.recruiting = false;
+        }
+
+        if (boat.shipState === 4 && boat.departureTime > 0 && (now - boat.lastMoved) > 1000) {
             boat.departureTime--;
             boat.lastMoved = new Date();
-
             if (boat.departureTime <= 0) {
                 boat.exitIsland();
 
-                // Make all krew members close their shopping windows.
-                for (let i in boat.children) {
+                // make all krew members close their shopping windows
+                for (i in boat.children) {
                     let boatMember = boat.children[i];
-                    if (boatMember != undefined && boatMember.netType == 0) {
-                        boatMember.socket.emit(`exitIsland`, {
+                    if (boatMember !== undefined && boatMember.netType === 0) {
+                        boatMember.socket.emit('exitIsland', {
                             captainId: boat.captainId
                         });
-                        boatMember.rareItemsFound = [];
-
                         boatMember.sentDockingMsg = false;
                         boatMember.checkedItemsList = false;
+                        boatMember.rareItemsFound = [];
                     }
                 }
             }
         }
+
         if (boat.krewCount > 0) {
             let boatScoreObj = {
                 id: boat.id,
                 cN: boat.crewName,
-                c: entities[boat.captainId] != undefined ? entities[boat.captainId].clan : ``,
+                c: entities[boat.captainId] !== undefined ? entities[boat.captainId].clan : "",
                 players: [],
+                //salary: 0,
                 s: 0,
                 g: 0,
                 cI: boat.captainId,
-                ok: boat.overallKills,
-                oc: boat.overallCargo,
-                oql: boat.otherQuestLevel
-            }
-            for (let i in boat.children) {
-                let player = boat.children[i];
+                ok: boat.overall_kills,
+                oc: boat.overall_cargo,
+                oql: boat.other_quest_level,
+            };
+            for (let id in boat.children) {
                 let playerObj = {
-                    id: player.id,
-                    name: player.name,
-                    salary: parseFloat(boat.salary).toFixed(0),
-                    score: parseFloat(boat.score).toFixed(0),
-                    parentId: boat.parent.id,
-                    gold: parseFloat(boat.gold).toFixed(0),
-                    cargoused: boat.cargoUsed
-                }
-                if (boatScoreObj.players.length > 0) scores.boats.push(boatScoreObj);
+                    id: boat.children[id].id,
+                    name: boat.children[id].name,
+                    salary: parseFloat(boat.children[id].salary).toFixed(0),
+                    score: parseFloat(boat.children[id].score).toFixed(0),
+                    parentId: boat.children[id].parent.id,
+                    gold: parseFloat(boat.children[id].gold).toFixed(0),
+                    cargoUsed: boat.children[id].cargoUsed,
+                };
+                //boatScoreObj.salary += parseInt(playerObj.salary);
+                boatScoreObj.s += parseInt(playerObj.score);
+                boatScoreObj.g += parseInt(playerObj.gold);
+                boatScoreObj.players.push(playerObj);
             }
+
+            if (boatScoreObj.players.length > 0) {
+                scores.boats.push(boatScoreObj);
+            }
+
         }
+
     }
 
     for (let i in core.Landmarks) {
         let landmark = core.Landmarks[i];
+        if (landmark.pickups === undefined) {
+            landmark.pickups = {};
+        }
 
-        if (landmark.pickups == undefined) landmark.pickups = {}
-        for (let i in landmark.pickups) {
-            let pickup = landmark.pickups[i];
-            if (pickup == undefined) delete pickup;
+        for (let pickupId in landmark.pickups) {
+            if (core.entities[pickupId] === undefined) {
+                delete landmark.pickups[pickupId];
+            }
         }
 
         while (Object.keys(landmark.pickups).length < 20) {
@@ -144,12 +182,26 @@ setInterval(() => {
             let pickupPosition = {
                 x: 0,
                 z: 0
-            }
+            };
             let distanceFromCenter = 0;
 
-            while (distanceFromCenter > landmark.dockRadius - 30 || distanceFromCenter < landmark.dockRadius) {
-                pickupPosition.x = Math.floor(Math.random() * ((landmark.position.x + landmark.dockRadius) - (landmark.position.x - landmark.dockRadius)) + landmark.position.x - landmark.dockRadius);
-                pickupPosition.z = Math.floor(Math.random() * ((landmark.position.z + landmark.dockRadius) - (landmark.position.z - landmark.dockRadius)) + landmark.position.z - landmark.dockRadius);
+            while (
+                distanceFromCenter > landmark.dockRadius - 30 ||
+                distanceFromCenter < landmark.dockRadius - 40
+            ) {
+                pickupPosition.x = Math.floor(
+                    Math.random() * (
+                        (landmark.position.x + landmark.dockRadius) -
+                        (landmark.position.x - landmark.dockRadius)
+                    )
+                ) + (landmark.position.x - landmark.dockRadius);
+
+                pickupPosition.z = Math.floor(
+                    Math.random() * (
+                        (landmark.position.z + landmark.dockRadius) -
+                        (landmark.position.z - landmark.dockRadius)
+                    )
+                ) + (landmark.position.z - landmark.dockRadius);
 
                 distanceFromCenter = Math.sqrt(
                     (pickupPosition.x - landmark.position.x) *
@@ -162,29 +214,53 @@ setInterval(() => {
             let pickup = core.createPickup(size, pickupPosition.x, pickupPosition.z, type, false);
             landmark.pickups[pickup.id] = pickup;
         }
+
     }
 
-    // Fill up the world to the brink with supplies.
-    let pickupAmount = Object.keys(core.pickups).length;
+    // fill up the world to the brink with supplies
+    let pickUpAmount = Object.keys(core.pickups).length;
     amountChests = 0;
+    for (x in core.pickups) {
+        if (core.pickups[x].type === 4) {
+            amountChests++;
+        }
+    }
+    if (pickUpAmount > maxAmountCratesInSea) {
+        for (p in core.pickups) {
+            let pickup = core.pickups[p];
+            core.removeEntity(pickup);
+        }
+    }
 
-    for (let i in core.pickups)
-        if (core.pickups[i].type == 4) amountChests++;
-
-    while (pickupAmount < cratesInSea.min) {
-        // Constant amount of crates at sea.
-        pickupAmount++;
-
+    while (pickUpAmount < minAmountCratesInSea) { //Constant amount of crates at sea
+        ++pickUpAmount;
         let size = 2;
+        //random crates size
+        //let roll = Math.random();
+        //if (roll > 0.6) { size = 1; }
+        //if (roll > 0.9) { size = 2; }
+
         core.createPickup(size, core.worldsize * Math.random(), core.worldsize * Math.random(), 0, false);
     }
 
-    if (amountChests == 0) {
-        if (respawnChestsDate < Date.now()) core.createPickup(4, core.worldsize * Math.random(), core.worldsize * Math.random(), 4, false, (Math.random() * 5e4) + 1e4);
-        else if (!respawnChestsDate) respawnChestsDate = Date.now() + (Math.random() * 6e5) + 3e5;
+    if (amountChests === 0) {
+        if (respawnChestsDate < Date.now()) {
+            let size = 4;
+            core.createPickup(size, core.worldsize * Math.random(), core.worldsize * Math.random(), 4, false, (10000 + Math.random() * (60000 - 10000)));
+            io.emit('showCenterMessage', 'The old pirate threw his treasure chest! Hurry to pick up the gold first!', 4, 5000);
+            respawnChestsDate = undefined;
+        } else {
+            if (respawnChestsDate === undefined) {
+                respawnChestsDate = Date.now() + 300000 + Math.random() * (900000 - 300000);
+            }
+        }
     }
 
-    // Compress the snapshot.
+    // compress the snapshot data with lz-string
     scores = lzString.compress(JSON.stringify(scores));
-    socket.io.emit(`scores`, scores);
-}, 1e3);
+
+    socket.io.emit('scores', scores);
+
+    // console.log(Object.keys(core.entities).length);
+
+}, 1000);
