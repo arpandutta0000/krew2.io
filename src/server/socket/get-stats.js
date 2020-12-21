e13b5ae2ebadbf7e8c23eb2d8b9eb2d311110678
BR88C <bcroethel88@gmail.com>
2020-12-21 13:08:37 -0500

    fix event handler and add chat message

--- src/server/socket/get-stats.js
+++ src/server/socket/get-stats.js
@@ -6,12 +6,17 @@ const login = require(`../auth/login.js`);
 const md5 = require(`../utils/md5.js`);
 const thugConfig = require(`../config/thugConfig`);
 const xssFilters = require(`xss-filters`);
+const {
+    isSpamming,
+    mutePlayer,
+    charLimit
+} = require(`../utils/chat.js`);
 
-let User = require(`./models/user.model.js`);
-let Clan = require(`./models/clan.model.js`);
-let Ban = require(`./models/ban.model.js`);
-let Hacker = require(`./models/hacker.model.js`);
-let PlayerRestore = require(`./models/playerRestore.model.js`);
+let User = require(`../models/user.model.js`);
+let Clan = require(`../models/clan.model.js`);
+let Ban = require(`../models/ban.model.js`);
+let Hacker = require(`../models/hacker.model.js`);
+let PlayerRestore = require(`../models/playerRestore.model.js`);
 
 let {
     checkPlayerStatus,
@@ -20,11 +25,9 @@ let {
     filter,
     gameCookies,
     initSocketForPlayer,
-    krewIoData,
-    msgData,
+    krewioData,
     playerEntity,
     reportedIps,
-    server
 } = require(`../socketForClients.js`);
 
 
