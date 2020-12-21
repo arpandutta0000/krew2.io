bda5c58bd8e7c0308180748715b989c792a6b166
Damien Vesper <ldamienvesper@gmail.com>
2020-12-21 08:02:57 -0600

    better looking es6 for itemtypes

--- src/server/core/core_item_types.js
+++ src/server/core/core_item_types.js
@@ -6,51 +6,51 @@ let itemTypes = [{
         id: 2,
         name: `Sinker's Gloves`,
         Description: `+25 cannon fire rate`,
-        price: 45000,
+        price: 45e3,
         rarity: 0.2,
         availableAt: [`Spain`, `Brazil`],
         attributes: {
-            attackSpeed: `25`,
-        },
+            attackSpeed: `25`
+        }
     },
     {
         id: 3,
         name: `Steel Barrel`,
         Description: `+30 cannon distance`,
-        price: 35000,
+        price: 35e3,
         rarity: 0.6,
         availableAt: [`Labrador`],
         attributes: {
-            attackDistance: `30`,
-        },
+            attackDistance: `30`
+        }
     },
     {
         id: 4,
         name: `Air Pegleg`,
         Description: `+1 ship speed (only works if you are captain)`,
-        price: 22000,
+        price: 22e3,
         rarity: 0.3,
         availableAt: [`Jamaica`],
         attributes: {
-            movementSpeed: `100`,
-        },
+            movementSpeed: `100`
+        }
     },
     {
         id: 5,
         name: `Blue Gunpowder`,
         Description: `+8 cannon damage`,
-        price: 50000,
+        price: 5e4,
         rarity: 0.25,
         availableAt: [`Jamaica`],
         attributes: {
             attackDamage: `8`,
-        },
+        }
     },
     {
         id: 6,
         name: `Cannon distance upgrade`,
         Description: `+5 cannon distance`,
-        price: 4000,
+        price: 4e3,
         rarity: 1,
         attributes: {
             attackDistance: `5`
@@ -60,7 +60,7 @@ let itemTypes = [{
         id: 7,
         name: `Attack speed upgrade`,
         Description: `+5 cannon fire rate`,
-        price: 2000,
+        price: 2e3,
         rarity: 1,
         attributes: {
             attackSpeed: `5`
@@ -70,7 +70,7 @@ let itemTypes = [{
         id: 8,
         name: `Damage upgrade`,
         Description: `+5 cannon damage`,
-        price: 5000,
+        price: 5e3,
         rarity: 1,
         attributes: {
             attackDamage: `5`
@@ -80,7 +80,7 @@ let itemTypes = [{
         id: 9,
         name: `Ship Speed Upgrade`,
         Description: `+0.2 ship speed (only works if you are captain)`,
-        price: 3000,
+        price: 3e3,
         rarity: 1,
         attributes: {
             movementSpeed: `20`
@@ -90,7 +90,7 @@ let itemTypes = [{
         id: 10,
         name: `Bruiser`,
         Description: `+2 cannon damage<br/>+10 cannon fire rate`,
-        price: 20000,
+        price: 2e4,
         rarity: 0.35,
         availableAt: [`Spain`, `Brazil`],
         attributes: {
@@ -101,8 +101,8 @@ let itemTypes = [{
     {
         id: 11,
         name: `Demolisher`,
-        Description: `+4 cannon damage<br/>+25 cannon fire rate<br/><br/>Requirements:<br/>- Sink 10 ships<br/>- Trade goods worth 100,000 gold`,
-        price: 100000,
+        Description: `+4 cannon damage<br/>+25 cannon fire rate<br/><br/>Requirements:<br/>- Sink 10 ships<br/>- Trade goods worth 100,e3 gold`,
+        price: 1e5,
         rarity: 1,
         availableAt: [`Jamaica`],
         attributes: {
@@ -114,7 +114,7 @@ let itemTypes = [{
         id: 12,
         name: `Drifter`,
         Description: `+2 cannon damage<br/>+0.5 ship speed (only works if you are captain)`,
-        price: 25000,
+        price: 25e3,
         rarity: 0.45,
         availableAt: [`Guinea`, `Labrador`],
         attributes: {
@@ -126,7 +126,7 @@ let itemTypes = [{
         id: 13,
         name: `Reinforced Planks`,
         Description: `+25% to protect your ship (only works if you are captain)`,
-        price: 35000,
+        price: 35e3,
         rarity: 0.45,
         availableAt: [`Brazil`],
         attributes: {
@@ -137,8 +137,8 @@ let itemTypes = [{
         id: 14,
         name: `Fountain of youth`,
         Description: `New chance to allocate all your skill points.<br/>Can only be bought once`,
-        price: 150000,
+        price: 15e4,
         rarity: 0.33,
-        availableAt: [`Jamaica`],
-    },
+        availableAt: [`Jamaica`]
+    }
 ];
