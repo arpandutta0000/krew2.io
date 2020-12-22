a6d210dcca998b2cc5249eab2928460c55b47cb4
Damien Vesper <ldamienvesper@gmail.com>
2020-12-22 12:29:33 -0600

    MAYBE WORK FOR WEBPACK

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,53 @@ const dotenv = require(`dotenv`).config();
 
 module.exports = {
     mode: `production`,
-    entry: path.resolve(__dirname, `dist/script/dist.js`),
+    entry: [
+        `./src/client/script/core/core_client/config.js`,
+        `./src/client/script/rangeInput.js`,
+
+        `./src/client/assets/js/canvas.map.js`,
+        `./src/client/libs/ua.js`,
+        `./src/client/libs/keypress.min.js`,
+        `./src/client/libs/OBJLoader.js`,
+        `./src/client/libs/TGALoader.js`,
+        `src/lcient/libs/MTLLoader/js`,
+        `./src/client/libs/socket.js`,
+
+        `./src/client/script/environment.js`,
+        `./src/client/script/window.js`,
+        `./src/client/script/geometry.js`,
+        `./src/client/script/loader.js`,
+        `./src/client/script/keyboard.js`,
+        `./src/client/script/controls.js`,
+
+        `./src/client/script/core/core.js`,
+        `./src/client/script/core/utils.js`,
+        `./src/client/script/core/entity.js`,
+        `./src/client/script/core/goodsTypes.js`,
+        `./src/client/script/core/client/parseSnap.js`,
+        `./src/client/script/core/boatTypes.js`,
+        `./src/client/script/core/boat.js`,
+        `./src/client/script/core/item.js`,
+        `./src/client/script/core/player.js`,
+        `./src/client/script/core/impact.js`,
+        `./src/client/script/core/pickup.js`,
+        `./src/client/script/core/landmark.js`,
+        `./src/client/script/core/projectile.js`,
+
+        `./src/client/script/core/core_client/entity.js`,
+        `./src/client/script/core/core_client/boat.js`,
+        `./src/client/script/core/core_client/player.js`,
+
+        `./src/client/script/uiSuggestion.js`,
+        `./src/client/script/uiKrewList.js`,
+        `./src/client/script/uiGoods.js`,
+        `./src/client/script/uiExperience.js`,
+
+        `./src/client/script/ui.js`,
+        `./src/client/script/main.js`,
+        `./src/client/script/particles.js`,
+        `./src/client/script/connection.js`
+    ],
     output: {
         path: path.resolve(__dirname, `dist/script`),
         filename: `dist.min.js`
