7a3ef42fb9866936815df72422c46f79f9c9d32d
BR88C <bcroethel88@gmail.com>
2020-12-22 10:28:42 -0500

    push pm2 changes

--- krew2.js
+++ krew2.js
@@ -1,8 +1,9 @@
 module.exports = {
     apps: [{
         name: `krew2`,
-        script: `../app.js`,
-        node_args: `--p=443`,
+        script: `./src/server/app.js`,
+        out_file: `./logs/outlog`,
+        error_file: `./logs/err.log`,
         env_prod: {
             NODE_ENV: `prod`,
         }
