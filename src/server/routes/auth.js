b577b44e8ed1363fd46c43483251f79d23bbf764
BR88C <bcroethel88@gmail.com>
2020-12-23 10:53:00 -0500

    add username checks

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -5,27 +5,40 @@ const log = require(`../utils/log.js`);
 
 const express = require(`express`);
 let router = express.Router();
+const xssFilters = require(`xss-filters`);
 
 // Authentication.
 const User = require(`../models/user.model.js`);
 const passport = require(`passport`);
 
 router.post(`/register`, (req, res, next) => {
-    if (req.body[`register-password`] != req.body[`register-password-confirm`]) return res.json({
-        errors: `Passwords do not match`
-    });
-
     if (!req.body[`register-username`] || !req.body[`register-email`] || !req.body[`register-password`] || !req.body[`register-password-confirm`] ||
         typeof req.body[`register-username`] != `string` || typeof req.body[`register-email`] != `string` || typeof req.body[`register-password`] != `string` || typeof req.body[`register-password-confirm`] != `string`) return res.json({
-        errors: `Bad request`
+        errors: `Please fill out all fields`
+    });
+
+    if (req.body[`register-password`] < 7 || req.body[`register-password`] > 48) return res.json({
+        errors: `Password must be between 7 and 48 characters`
+    });
+
+    if(!/[a-zA-Z]/.test(req.body[`register-username`])) return res.json({
+        errors: `Your username must contain at least one letter`
+    });
+
+    if(req.body[`register-username`] != xssFilters.inHTMLData(req.body[`register-username`]) || /[^\w\s]/.test(req.body[`register-username`])) return res.json({
+        errors: `Invalid Username`
     });
 
     if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(req.body[`register-email`])) return res.json({
         errors: `Invalid email`
     });
 
-    if (req.body[`register-password`] < 7 || req.body[`register-password`] > 48) return res.json({
-        errors: `Password must be between 7 and 48 characters`
+    if(req.body[`register-password`] != xssFilters.inHTMLData(req.body[`register-password`])) return res.json({
+        errors: `Invalid Password`
+    });
+
+    if (req.body[`register-password`] != req.body[`register-password-confirm`]) return res.json({
+        errors: `Passwords do not match`
     });
 
     passport.authenticate(`register`, (err, user, info) => {
@@ -53,7 +66,7 @@ router.post(`/login`, (req, res, next) => {
     }
     if (!req.body[`login-user`] || !req.body[`login-password`] ||
         typeof req.body[`login-user`] != `string` || typeof req.body[`login-password`] != `string`) return res.json({
-        errors: `Bad request`
+        errors: `Please fill out all fields`
     });
 
     passport.authenticate(`login`, (err, user, info) => {
