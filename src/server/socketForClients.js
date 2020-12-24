d6b248b72bb0e1974476036a1c3a8a8189d360fb
Damien Vesper <ldamienvesper@gmail.com>
2020-12-23 21:07:29 -0600

    fix bans

--- src/server/socketForClients.js
+++ src/server/socketForClients.js
@@ -393,6 +393,7 @@ io.on(`connection`, async socket => {
                         });
 
                         ban.save(() => {
+                            player.socket.emit(`showCenterMessage`, `You have been banned!`, 1, 6e4);
                             player.socket.disconnect();
                             playerEntity.socket.emit(`showCenterMessage`, `You permanently banned ${player.name}`, 3, 1e4);
                         });
@@ -415,6 +416,7 @@ io.on(`connection`, async socket => {
                         });
 
                         ban.save(() => {
+                            player.socket.emit(`showCenterMessage`, `You have been banned for 1 hour.`, 1, 6e4);
                             player.socket.disconnect();
                             playerEntity.socket.emit(`showCenterMessage`, `You temporarily banned ${player.name}`, 3);
                         });
