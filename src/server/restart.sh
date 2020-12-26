55e3b20e331dadafbbbff48553e41fa5f6764822
Damien Vesper <ldamienvesper@gmail.com>
2020-12-26 12:33:29 -0600

    pull instead of clone

--- src/server/restart.sh
+++ src/server/restart.sh
@@ -1,6 +1,4 @@
-cd /opt
-rm -r krew2.io
-git clone --depth=1 https://Krewio:QqstZahYs3kUzF9du3Yq9rM43VuLR2zt@github.com/Krew-io/krew2.io.git
-cd krew2.io
+cd /opt/krew2.io
+git pull
 npm i
 pm2 restart all
