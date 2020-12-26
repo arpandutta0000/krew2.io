32dc867db9a6cc68bd54ce22fc6c10422502a07b
Damien Vesper <ldamienvesper@gmail.com>
2020-12-26 12:16:20 -0600

    change pass

--- src/server/restart.sh
+++ src/server/restart.sh
@@ -1,6 +1,6 @@
 cd /opt
 rm -r krew2.io
-git clone --depth=1 https://Krewio:2r@DD%X=m@W%ccD_GmL_2enK3sRWHU@github.com/Krew-io/krew2.io.git
+git clone --depth=1 https://Krewio:QqstZahYs3kUzF9du3Yq9rM43VuLR2zt@github.com/Krew-io/krew2.io.git
 cd krew2.io
 npm i
 pm2 restart all
