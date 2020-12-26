f5989bc21aecfa4e9dc1b2bdc73a003cbff2be2b
Damien Vesper <ldamienvesper@gmail.com>
2020-12-26 17:18:30 -0600

    Revert "bye mongo"
    This reverts commit e9a539146a50cfe2718510f3afc0e8f933c479c6.

--- src/server/server.js
+++ src/server/server.js
@@ -26,7 +26,7 @@ const passport = require(`./passport.js`);
 // Database connection.
 const MongoStore = require(`connect-mongo`)(session);
 const mongoose = require(`mongoose`);
-mongoose.connect(`mongodb://localhost:27017/localKrewDB`, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => log(`green`, `User authentication has connected to database.`));
