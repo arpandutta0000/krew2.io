41e78da9d3d129b1c1757404945999908608543d
BR88C <bcroethel88@gmail.com>
2020-12-26 19:06:24 -0500

    Update restart.sh

--- src/server/restart.sh
+++ src/server/restart.sh
@@ -2,5 +2,5 @@ cd /opt
 sudo rm -r krew2.io
 sudo git clone --depth=1 https://Krewio:QqstZahYs3kUzF9du3Yq9rM43VuLR2zt@github.com/Krew-io/krew2.io.git
 cd /opt/krew2.io
-sudo npm i
+npm i
 pm2 restart all
