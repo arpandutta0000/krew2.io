ba2d40e3491c2438754f09bbae80b79dc9f51ce2
BR88C <bcroethel88@gmail.com>
2020-12-28 21:39:40 -0500

    fix regex

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -33,7 +33,7 @@ const config = {
     devs: [],
     maxAmountCratesInSea: 1100,
     minAmountCratesInSea: 480,
-    whitespaceRegex: /"  *"|\b|\t|\n|\v|\f|\r|\040|\u0008|\u0009|\u000A|\u000B|\u000C|\u000D|\u0020|\u0022|\u0027|\u005C|\u00A0|\u2028|\u2029|\uFEFF/g
+    whitespaceRegex: /"  *"|\t|\n|\v|\f|\r|\040|\u0008|\u0009|\u000A|\u000B|\u000C|\u000D|\u0020|\u0022|\u0027|\u005C|\u00A0|\u2028|\u2029|\uFEFF/g
 }
 
 config.staticDir = config.mode == `dev` ? `${__dirname}/../../client/` : `${__dirname}/../../../dist/`;
