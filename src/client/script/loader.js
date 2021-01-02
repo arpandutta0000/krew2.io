11f5fc131006d5351e384f5dc3ee74f98cf432ee
Damien Vesper <ldamienvesper@gmail.com>
2021-01-02 14:49:49 -0600

    change loader

--- src/client/script/loader.js
+++ src/client/script/loader.js
@@ -1,156 +1,132 @@
-/* Texture loader, container ************************************************** */
-var textureLoader = new THREE.TextureLoader();
-var fileLoader = new THREE.FileLoader();
-var objLoader = new THREE.OBJLoader();
-var tgaloader = new THREE.TGALoader();
-var mtlloader = new THREE.MTLLoader();
-var textures = {};
-var shaders = {};
-var models = {};
-var json = {};
+// Texture loader, container.
+let textureLoader = new THREE.TextureLoader();
+let fileLoader = new THREE.FileLoader();
+let objLoader = new THREE.OBJLoader();
+let tgaLoader = new THREE.TGALoader();
+let mtlLoader = new THREE.MTLLoader();
 
-/* generic loader and async handler ******************************************* */
-var loader = {};
-loader.promises = [];
+let textures = {}
+let shaders = {}
+let models = {}
+let json = {}
 
-// add a task to loader. can be any function, request will be solved once function was called
-loader.compute = function (fun) {
-    loader.promises.push(new Promise(function (resolve, reject) {
+// Generic loader and async handler.
+let loader = {
+    promises: []
+}
+
+// Add a task to loader. Can be any function, request will be solved once function was called.
+loader.compute = fun => {
+    loader.promises.push(new Promise((resolve, reject) => {
         fun();
-
-        //console.log("computed: ")
         resolve();
     }));
-
     return loader.promises[loader.promises.length - 1];
-};
+}
 
-// add a texture load to the loader. wraps the async calls
-loader.loadTexture = function (path) {
-    loader.promises.push(new Promise(function (resolve, reject) {
-        var parts = path.split('/').pop().split('.');
-        var ext = parts.pop();
-        var name = parts.pop();
+// Add a texture load to the loader. Wraps the async calls.
+loader.loadTexture = path => {
+    loader.promises.push(new Promise((resolve, reject) => {
+        let parts = path.split(`/`).pop().split(`.`)
+        let ext = parts.pop();
+        let name = parts.pop();
 
-        if (ext === 'tga') {
-            tgaloader.load(path, function (texture) {
+        if(ext == `tga`) {
+            tgaLoader.load(path, texture => {
                 textures[name] = texture;
                 resolve();
             });
-
             return;
         }
-
-        if (ext === 'mtl') {
-            var folder = path.split('/');
+        else if(ext == `tga`) {
+            let folder = path.split(`/`);
             folder.pop();
-            folder = folder.join('/') + '/';
-            mtlloader.setPath(folder);
-            mtlloader.load(name + '.' + ext, function (materials) {
+            folder = folder.join(`/`) + `/`;
+            mtlLoader.setPath(folder);
+            mtlLoader.load(`${name}.${ext}`, materials => {
                 materials.preload();
                 objLoader.setMaterials(materials);
                 resolve();
             });
-
             return;
         }
-
-        textureLoader.load(
-            path, // resource URL
-            function (texture) { // Function when resource is loaded
-                textures[path.substring(path.lastIndexOf('/') + 1, path.length).replace(/\.[^/.]+$/, '')] = texture;
+        textureLoader.load(path, // Resource URL.
+            texture => {
+                // When resource is loaded.
+                textures[path.substring(path.lastIndexOf(`/`) + 1, path.length).replace(/\.[^/.]+$/, ``)] = texture;
                 resolve();
             },
+            xhr => {}
+        );
+    }));
+}
 
-            function (xhr) { // Function called when download progresses
-                //console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
-            },
-
-            // Function called when download errors
-            function (xhr) {
-                //console.log( 'An error happened' );
+// Add a model load to the loader. Wraps the async calls.
+loader.loadModel = path => {
+    loader.promises.push(new Promise((resolve, reject) => {
+        objLoader.load(path, // Resource URL.
+            object => {
+                // When resource is loaded.
+                models[path.substring(path.lastIndexOf(`/`) + 1, path.length).replace(/\.[^/.]+$/, ``)] = object;
+                resolve();
             }
         );
-
     }));
-
     return loader.promises[loader.promises.length - 1];
-};
+}
 
-// add a texture load to the loader. wraps the async calls
-loader.loadModel = function (path) {
-    loader.promises.push(new Promise(function (resolve, reject) {
-        objLoader.load(
-            path, // resource URL
-            function (object) { // Function when resource is loaded
-                models[path.substring(path.lastIndexOf('/') + 1, path.length).replace(/\.[^/.]+$/, '')] = object;
-                resolve();
-            });
+// Add an object tile load to the loader. Wraps the async calls.
+loader.loadObjWithMtl = path => {
+    let objLoader = new THREE.OBJLoader();
+    let mtlLoader = new THREE.MTLLoader();
 
-    }));
+    let folder = path.split(`/`);
+    let parts = folder.pop().split(`.`);
+    let ext = parts.pop();
+    let name = parts.pop();
 
-    return loader.promises[loader.promises.length - 1];
-};
+    folder = folder.join(`/`) + `/`;
 
-// add a texture load to the loader. wraps the async calls
-loader.loadObjWithMtl = function (path) {
-    var objLoader = new THREE.OBJLoader();
-    var mtlloader = new THREE.MTLLoader();
-    var folder = path.split('/');
-    var parts = folder.pop().split('.');
-    var ext = parts.pop();
-    var name = parts.pop();
-
-    folder = folder.join('/') + '/';
-
-    loader.promises.push(new Promise(function (resolve, reject) {
-
-        mtlloader.setPath(folder);
-        mtlloader.load(name + '.mtl', function (materials) {
+    loader.promises.push(new Promise((resolve, reject) => {
+        mtlLoader.setPath(folder);
+        mtlLoader.load(`${name}.mtl`, materials => {
             materials.preload();
             objLoader.setMaterials(materials);
-            objLoader.load(path, function (object) {
-                models[path.substring(path.lastIndexOf('/') + 1, path.length).replace(/\.[^/.]+$/, '')] = object;
+            objLoader.load(path, object => {
+                models[path.substring(path.lastIndexOf(`/`) + 1, path.length).replace(/\.[^/.]+$/, ``)] = object;
                 resolve();
             });
         });
-
     }));
-
     return loader.promises[loader.promises.length - 1];
-};
+}
 
-// add a texture load to the loader. wraps the async calls
-loader.loadShader = function (path) {
-    loader.promises.push(new Promise(function (resolve, reject) {
-        fileLoader.load(
-            path, // resource URL
-            function (data) { // Function when resource is loaded
-                shaders[path.substring(path.lastIndexOf('/') + 1, path.length)] = data;
+// Add a shader load to the loader. Wraps the async calls.
+loader.loadShader = path => {
+    loader.promises.push(new Promise((resolve, reject) => {
+        fileLoader.load(path, // Resource URL.
+            data => {
+                // When resource is loaded.
+                shaders[path.substring(path.lastIndexOf(`/`) + 1, path.length)] = data;
                 resolve();
             }
         );
-
     }));
-
     return loader.promises[loader.promises.length - 1];
-};
+}
 
-loader.loadJSON = function (path) {
-    loader.promises.push(new Promise(function (resolve, reject) {
-        $.getJSON(path, function (data) {
-            json[path.substring(path.lastIndexOf('/') + 1, path.length).replace(/\.[^/.]+$/, '')] = data;
+loader.loadJSON = path => {
+    loader.promises.push(new Promise((resolve, reject) => {
+        $.getJSON(path, data => {
+            json[path.substring(path.lastIndexOf(`/`) + 1, path.length).replace(/\.[^/.]+$/, ``)] = data;
             resolve();
         });
     }));
-
     return loader.promises[loader.promises.length - 1];
-};
+}
 
-loader.onFinish = function (fn) {
-    Promise.all(loader.promises).then(function (results) {
-        if (fn) {
-            fn();
-        }
+loader.onFinish = fn => {
+    Promise.all(loader.promises).then(results => {
+        if(fn) fn();
     });
-};
+}
