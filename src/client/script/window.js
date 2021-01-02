a9c00624efa335175a6a17fdbaf32c84c47eaafe
Damien Vesper <ldamienvesper@gmail.com>
2021-01-02 14:45:52 -0600

    es6

--- src/client/script/window.js
+++ src/client/script/window.js
@@ -1,13 +1,10 @@
-// Automatically resize renderer when the window is resized
-var updateViewport = function () {
-    if (renderer) {
-        renderer.setSize(window.innerWidth, window.innerHeight);
-    }
-
+// Automatically resize renderer when the window is resized.
+let updateViewport = () => {
+    if (renderer) renderer.setSize(window.innerWidth, window.innerHeight);
     if (camera) {
         camera.aspect = window.innerWidth / window.innerHeight;
         camera.updateProjectionMatrix();
     }
-};
+}
 
-window.addEventListener('resize', updateViewport, false);
+window.addEventListener(`resize`, updateViewport, false);
