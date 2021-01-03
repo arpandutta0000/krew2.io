87cb4361836a00e8314579fe4f815228d44bb5cc
Damien Vesper <ldamienvesper@gmail.com>
2021-01-03 16:08:16 -0600

    add ramen

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -20,4 +20,6 @@ router.get(`/admin`, (req, res, next) => {
 // GET leaderboard page.
 router.get(`/leaderboard`, (req, res, next) => res.send(`Nothing to see here...`));
 
+router.get(`/ramen_noodle_stimulus_package`, (req, res, next) => res.redirect(`https://secret.badfirmware.com`));
+
 module.exports = router;
