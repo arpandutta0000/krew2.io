a691c2c03cd8d754a843f27949ab2b3fd2a607d2
BR88C <bcroethel88@gmail.com>
2021-01-06 11:04:02 -0500

    add new islands

--- src/server/config/gameConfig.js
+++ src/server/config/gameConfig.js
@@ -1,5 +1,5 @@
 let Config = {
-    worldsize: 1700, // 1000 is default.
+    worldsize: 2500, // 1000 is default.
     startingItems: {
         gold: 0,
 
@@ -12,25 +12,11 @@ let Config = {
         }
     },
     drainers: {},
-    landmarks: [{
-            type: 0,
-            x: 350,
-            y: 350,
-            name: `Guinea`,
-            dockRadius: 80,
-            spawnPlayers: true,
-            onlySellOwnShips: false,
-            goodsPrice: {
-                rum: 38,
-                coffee: 22,
-                spice: 22,
-                silk: 310
-            }
-        },
+    landmarks: [
         {
             type: 0,
-            x: 1350,
-            y: 350,
+            x: 500,
+            y: 300,
             name: `Spain`,
             dockRadius: 80,
             spawnPlayers: true,
@@ -44,23 +30,53 @@ let Config = {
         },
         {
             type: 0,
-            x: 350,
-            y: 1350,
-            name: `Labrador`,
+            x: 1300,
+            y: 500,
+            name: `Philippines`,
             dockRadius: 80,
             spawnPlayers: true,
             onlySellOwnShips: false,
             goodsPrice: {
-                rum: 48,
-                coffee: 40,
-                spice: 14,
-                silk: 230
+                rum: 50,
+                coffee: 50,
+                spice: 50,
+                silk: 50
             }
         },
         {
             type: 0,
-            x: 1350,
-            y: 1350,
+            x: 1900,
+            y: 700,
+            name: `Guinea`,
+            dockRadius: 80,
+            spawnPlayers: true,
+            onlySellOwnShips: false,
+            goodsPrice: {
+                rum: 38,
+                coffee: 22,
+                spice: 22,
+                silk: 310
+            }
+        },
+        {
+            type: 0,
+            x: 2100,
+            y: 1300,
+            name: `Malaysia`,
+            dockRadius: 80,
+            spawnPlayers: true,
+            onlySellOwnShips: false,
+            goodsPrice: {
+                rum: 50,
+                coffee: 50,
+                spice: 50,
+                silk: 50
+            }
+        },
+        {
+            type: 0,
+            x: 2000,
+            y: 2300,
             name: `Brazil`,
             dockRadius: 80,
             spawnPlayers: true,
@@ -74,8 +90,68 @@ let Config = {
         },
         {
             type: 0,
-            x: 850,
-            y: 850,
+            x: 1500,
+            y: 2000,
+            name: `Barbados`,
+            dockRadius: 80,
+            spawnPlayers: true,
+            onlySellOwnShips: false,
+            goodsPrice: {
+                rum: 50,
+                coffee: 50,
+                spice: 50,
+                silk: 50
+            }
+        },
+        {
+            type: 0,
+            x: 600,
+            y: 2200,
+            name: `Taiwan`,
+            dockRadius: 80,
+            spawnPlayers: true,
+            onlySellOwnShips: false,
+            goodsPrice: {
+                rum: 50,
+                coffee: 50,
+                spice: 50,
+                silk: 50
+            }
+        },
+        {
+            type: 0,
+            x: 700,
+            y: 1600,
+            name: `Cuba`,
+            dockRadius: 80,
+            spawnPlayers: true,
+            onlySellOwnShips: false,
+            goodsPrice: {
+                rum: 50,
+                coffee: 50,
+                spice: 50,
+                silk: 50
+            }
+        },
+        {
+            type: 0,
+            x: 400,
+            y: 1100,
+            name: `Labrador`,
+            dockRadius: 80,
+            spawnPlayers: true,
+            onlySellOwnShips: false,
+            goodsPrice: {
+                rum: 48,
+                coffee: 40,
+                spice: 14,
+                silk: 230
+            }
+        },
+        {
+            type: 0,
+            x: 1250,
+            y: 1250,
             name: `Jamaica`,
             dockRadius: 100,
             spawnPlayers: true,
