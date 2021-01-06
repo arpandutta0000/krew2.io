138204198cfe9d0bbe8f34f4bff7b551b01b0b21
BR88C <bcroethel88@gmail.com>
2021-01-06 16:02:57 -0500

    remove drainRate (depreciated)

--- src/server/core/goodsTypes.js
+++ src/server/core/goodsTypes.js
@@ -1,30 +1,23 @@
 let goodsTypes = {
     rum: {
-        drainRate: 0,
         cargoSpace: 5
     },
     coffee: {
-        drainRate: 0,
         cargoSpace: 8
     },
     spice: {
-        drainRate: 0,
         cargoSpace: 8
     },
     silk: {
-        drainRate: 0,
         cargoSpace: 12
     },
     gems: {
-        drainRate: 0,
         cargoSpace: 15
     },
     sugar: {
-        drainRate: 0,
         cargoSpace: 15
     },
     bananas: {
-        drainRate: 0,
         cargoSpace: 5
     },
 };
