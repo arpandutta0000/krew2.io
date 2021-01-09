59417d63f3c48983c6b57e13f9ef3a18ed62b358
BR88C <bcroethel88@gmail.com>
2021-01-08 21:46:16 -0500

    fix whitespace regex

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -34,7 +34,7 @@ const config = {
     devs: [],
     maxAmountCratesInSea: 1100,
     minAmountCratesInSea: 480,
-    whitespaceRegex: /"  *"|\t|\n|\v|\f|\r|\040|\u0008|\u0009|\u000A|\u000B|\u000C|\u000D|\u0020|\u0022|\u0027|\u005C|\u00A0|\u2028|\u2029|\uFEFF/g
+    whitespaceRegex: /" *"|\t|\n|\v|\f|\r|\040|\u0008|\u0009|\u000A|\u000B|\u000C|\u000D|\u0020|\u0022|\u0027|\u005C|\u00A0|\u2028|\u2029|\uFEFF/g
 }
 
 config.staticDir = config.mode == `prod` ? `${__dirname}/../../../dist/` : `${__dirname}/../../client/`;
