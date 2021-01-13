9328555c99eceadc03f5624dc61ef50391a96818
BR88C <bcroethel88@gmail.com>
2021-01-13 09:44:50 -0500

    rename files and import core_client

--- src/client/scripts/config/clientConfig.js
+++ src/client/scripts/config/clientConfig.js
@@ -13,3 +13,25 @@ let config = {
     // Set worldsize
     worldsize: 2500,
 };
+
+/**
+ * This global variable, meant to hold all the posible configurations for the client.
+ * @type {Object}
+ */
+window.CONFIG = {
+    setProperties: {
+        inVision: false
+    },
+    Labels: {
+        redrawInterval: 250,
+        fontFamily: `Arial, Helvetica, sans-serif`,
+        distanceMultiplier: {
+            0: 40,
+            1: 160,
+            5: 300
+        },
+        boats: {
+            useMethod: `inRange` // `inVision` or `inRange`
+        }
+    }
+};
