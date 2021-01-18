3cffc366241916108f702f12457fc6cc748bd629
BR88C <bcroethel88@gmail.com>
2021-01-18 14:16:03 -0500

    fix

--- src/client/script/core/parseSnap.js
+++ src/client/script/core/parseSnap.js
@@ -67,42 +67,42 @@ let parseSnap = function (id, data) {
 };
 
 let entitySnap = {
-    parseSnap: (snap, id, __this) => {
-        if (snap.p && entities[snap.p] && __this.parent !== entities[snap.p]) {
+    parseSnap: (snap, id, _this) => {
+        if (snap.p && entities[snap.p] && _this.parent !== entities[snap.p]) {
             let oldPosition;
             let newparent = entities[snap.p];
-            let oldparent = __this.parent;
+            let oldparent = _this.parent;
             if (myPlayerId === id && newparent !== oldparent) {
                 ui.setActiveBtn(snap.p);
             }
             if (newparent.netType !== 5) {
                 if (
-                    __this.geometry !== undefined &&
+                    _this.geometry !== undefined &&
                     newparent.geometry !== undefined &&
                     oldparent &&
                     oldparent.geometry !== undefined
-                ) oldPosition = newparent.geometry.worldToLocal(oldparent.geometry.localToWorld(__this.geometry.position));
-                else oldPosition = newparent.toLocal(__this.worldPos());
+                ) oldPosition = newparent.geometry.worldToLocal(oldparent.geometry.localToWorld(_this.geometry.position));
+                else oldPosition = newparent.toLocal(_this.worldPos());
 
-                __this.position.x = oldPosition.x;
-                __this.position.y = oldPosition.y;
-                __this.position.z = oldPosition.z;
+                _this.position.x = oldPosition.x;
+                _this.position.y = oldPosition.y;
+                _this.position.z = oldPosition.z;
             }
 
-            newparent.addChildren(__this);
-            newparent.geometry.add(__this.geometry);
-            __this.geometry.position.set(__this.position.x, __this.position.y, __this.position.z);
+            newparent.addChildren(_this);
+            newparent.geometry.add(_this.geometry);
+            _this.geometry.position.set(_this.position.x, _this.position.y, _this.position.z);
 
             if (newparent.netType === 1) {
-                newparent.krewMembers[__this.id] = __this.geometry.children[0];
+                newparent.krewMembers[_this.id] = _this.geometry.children[0];
             }
             if (myPlayer && myPlayer.isCaptain === false && myPlayer.parent.netType === 5 && newparent.netType === 5 && oldparent !== undefined && oldparent.netType === 1 && oldparent.shipState === 1) {
                 $(`#abandon-ship-button`).hide();
                 showIslandMenu();
             }
 
-            if (__this.isPlayer && __this.parent && !__this.isCaptain && __this.parent.netType === 1) {
-                if (__this.parent.shipState === 3) {
+            if (_this.isPlayer && _this.parent && !_this.isCaptain && _this.parent.netType === 1) {
+                if (_this.parent.shipState === 3) {
                     $(`#exit-island-button`).hide();
                     $(`#invite-div`).hide();
                 }
@@ -112,58 +112,58 @@ let entitySnap = {
         }
 
         if (snap.t !== undefined) {
-            __this.parseTypeSnap(snap.t);
+            _this.parseTypeSnap(snap.t);
         }
 
-        if (!__this.isPlayer) {
+        if (!_this.isPlayer) {
             if (snap.x !== undefined) {
-                __this.position.x = parseFloat(snap.x);
+                _this.position.x = parseFloat(snap.x);
             }
 
             if (snap.y !== undefined) {
-                __this.position.y = parseFloat(snap.y);
+                _this.position.y = parseFloat(snap.y);
             }
 
             if (snap.z !== undefined) {
-                __this.position.z = parseFloat(snap.z);
+                _this.position.z = parseFloat(snap.z);
             }
 
             if (snap.r !== undefined) {
-                __this.rotation = parseFloat(snap.r);
+                _this.rotation = parseFloat(snap.r);
             }
         }
 
         // parse deletion packets
         if (snap.del !== undefined) {
-            __this.onDestroy();
-            delete entities[__this.id];
-            delete playerNames[__this.id];
+            _this.onDestroy();
+            delete entities[_this.id];
+            delete playerNames[_this.id];
         }
 
         // Update the player experience only when its needed
         if (snap.t !== undefined && snap.t.e !== undefined && snap.t.e !== null) {
-            if (snap.t.e.l !== undefined && snap.t.e.l !== __this.level) {
-                __this.level = parseInt(snap.t.e.l);
+            if (snap.t.e.l !== undefined && snap.t.e.l !== _this.level) {
+                _this.level = parseInt(snap.t.e.l);
             }
 
-            // Only do the computation if __this is the player
-            if (__this.isPlayer) {
-                if (snap.t.e.e !== undefined && snap.t.e.e !== __this.experience) {
-                    __this.experience = parseInt(snap.t.e.e);
-                    __this.experienceNeedsUpdate = true;
-                    __this.updateExperience();
+            // Only do the computation if _this is the player
+            if (_this.isPlayer) {
+                if (snap.t.e.e !== undefined && snap.t.e.e !== _this.experience) {
+                    _this.experience = parseInt(snap.t.e.e);
+                    _this.experienceNeedsUpdate = true;
+                    _this.updateExperience();
                 }
 
-                if (snap.t.e.p.fr !== undefined && snap.t.e.p.fr !== __this.points.fireRate) {
-                    __this.points.fireRate = parseInt(snap.t.e.p.fr);
+                if (snap.t.e.p.fr !== undefined && snap.t.e.p.fr !== _this.points.fireRate) {
+                    _this.points.fireRate = parseInt(snap.t.e.p.fr);
                 }
 
-                if (snap.t.e.p.ds !== undefined && snap.t.e.p.ds !== __this.points.distance) {
-                    __this.points.distance = parseInt(snap.t.e.p.ds);
+                if (snap.t.e.p.ds !== undefined && snap.t.e.p.ds !== _this.points.distance) {
+                    _this.points.distance = parseInt(snap.t.e.p.ds);
                 }
 
-                if (snap.t.e.p.dm !== undefined && snap.t.e.p.dm !== __this.points.damage) {
-                    __this.points.damage = parseInt(snap.t.e.p.dm);
+                if (snap.t.e.p.dm !== undefined && snap.t.e.p.dm !== _this.points.damage) {
+                    _this.points.damage = parseInt(snap.t.e.p.dm);
                 }
             }
         }
