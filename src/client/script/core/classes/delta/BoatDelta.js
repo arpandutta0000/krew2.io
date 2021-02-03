c85ce18db9fffa4bcfdf924e0938ef4d506baf3f
BR88C <bcroethel88@gmail.com>
2021-02-03 11:46:46 -0500

    Create BoatDelta.js

---
+++ src/client/script/core/classes/delta/BoatDelta.js
@@ -0,0 +1,23 @@
+let BoatDelta = {
+    getTypeDelta: (_this) => {
+        let delta = {
+            h: _this.deltaTypeCompare(`h`, _this.hp),
+            s: _this.deltaTypeCompare(`s`, _this.steering.toFixed(4)),
+            c: _this.deltaTypeCompare(`c`, _this.shipclassId),
+            b: _this.deltaTypeCompare(`b`, _this.captainId),
+            t: _this.deltaTypeCompare(`t`, _this.shipState),
+            a: _this.deltaTypeCompare(`a`, _this.anchorIslandId),
+            k: _this.deltaTypeCompare(`k`, _this.krewCount),
+            e: _this.deltaTypeCompare(`e`, _this.speed),
+            r: _this.deltaTypeCompare(`r`, _this.recruiting),
+            l: _this.deltaTypeCompare(`l`, _this.isLocked),
+            d: _this.deltaTypeCompare(`d`, _this.departureTime)
+        };
+
+        if (isEmpty(delta)) {
+            delta = undefined;
+        }
+
+        return delta;
+    }
+}
