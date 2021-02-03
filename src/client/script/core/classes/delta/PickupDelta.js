0aa8da3cd03e6dde9abb4f7f3430a7b5679e8c5b
BR88C <bcroethel88@gmail.com>
2021-02-03 12:18:03 -0500

    Create PickupDelta.js

---
+++ src/client/script/core/classes/delta/PickupDelta.js
@@ -0,0 +1,20 @@
+let PickupDelta = {
+    getTypeDelta: (_this) => {
+        if (_this.type === 1) {
+            if (!_this.spawnPacket) _this.spawnPacket = true;
+            return undefined;
+        } else {
+            let delta = {
+                s: _this.deltaTypeCompare(`s`, _this.pickupSize),
+                p: _this.deltaTypeCompare(`p`, _this.picking),
+                i: _this.deltaTypeCompare(`i`, _this.pickerId),
+                t: _this.deltaTypeCompare(`t`, _this.type)
+            };
+            if (isEmpty(delta)) {
+                delta = undefined;
+            }
+
+            return delta;
+        }
+    }
+};
