6394e842d22bb44ff2345a5f47aaac6d3cea4571
BR88C <bcroethel88@gmail.com>
2021-02-03 11:23:53 -0500

    modify landmark constructor

--- src/client/script/core/classes/Landmark.js
+++ src/client/script/core/classes/Landmark.js
@@ -1,75 +1,85 @@
+/* Landmark class */
+
 class Landmark extends Entity {
-    constructor(type, x, z, config) {
+    /* Constructor */
+    constructor(type, x, z, _config) {
+        // Inherit parent class methods
         super();
 
-        this.name = config.name || ``;
-
-        // netcode type
+        // Set netType
         this.netType = 5;
 
-        // landmark type
-        this.landmarkType = type;
-
-        // docking / anchoring ?
-        this.dockType = 1;
-        this.dockRadius = config.dockRadius;
-        this.spawnPlayers = config.spawnPlayers;
-        this.onlySellOwnShips = config.onlySellOwnShips;
-
-        // net data
+        // Set sending snap and delta
         this.sendDelta = false;
         this.sendSnap = false;
         this.sendCreationSnapOnDelta = true;
 
-        // // size of a Landmark
-        this.size = new THREE.Vector3(this.dockRadius, 20, this.dockRadius);
+        // Set landmark name
+        this.name = _config.name || ``;
+        this.setName(this.name);
 
+        // Set docking radius
+        this.dockRadius = _config.dockRadius;
+        this.collisionRadius = 30;
+
+        // Set landmark size and position
+        this.size = new THREE.Vector3(this.dockRadius, 20, this.dockRadius);
         this.position.x = x;
         this.position.z = z;
 
-        this.collisionRadius = 30;
-
-        // client side visuals
+        // Set landmark models
         this.baseGeometry = geometry.island;
         this.baseMaterial = materials.colorset;
 
-        // Palm Tree (Commented out for performance lol)
-        // if (this.name === 'Jamaica') {
-        //     this.palm = new THREE.Mesh(geometry.palm, materials.colorset);
-        //     this.palm.position.set(1250, 0, 1250);
-        //     this.palm.scale.x = 8;
-        //     this.palm.scale.y = 8;
-        //     this.palm.scale.z = 8;
-        //     this.palm.castShadow = true;
-        //     this.palm.receiveShadow = true;
-        //     scene.add(this.palm);
-        // }
-
-        // Christmas stuff (add snowman and christmas tree on Jamaica)
-        // models.elka.position.set(1240, 2, 1240);
-        // models.elka.scale.x = 0.35;
-        // models.elka.scale.y = 0.35;
-        // models.elka.scale.z = 0.35;
-        // models.snowman.position.set(1280, 38, 1285);
-        // models.snowman.rotation.set(0, -500, 0);
-        // models.snowman.scale.x = 0.17;
-        // models.snowman.scale.y = 0.17;
-        // models.snowman.scale.z = 0.17;
-        // scene.add(models.elka, models.snowman);
-
+        // Set model scale
         let modelscale = this.dockRadius / 10 / 8 * 9;
         this.modelscale.set(modelscale, modelscale, modelscale);
         this.modeloffset.set(0, 1, 0);
 
+        // Add docking ring
         this.visualCue = new THREE.Mesh(new THREE.RingBufferGeometry(this.dockRadius - 1, this.dockRadius, 30), materials.islandradius);
         this.visualCue.rotation.x = -Math.PI / 2;
-        // this.visualCue.scale.set(this.dockRadius, 1, this.dockRadius);
         this.visualCue.position.set(this.position.x, 1, this.position.z);
-        this.wavetimer = 0;
         scene.add(this.visualCue);
-        this.setName(this.name);
+
+        // Set misc values
+        this.landmarkType = type;
+        this.wavetimer = 0;
+
+        // Addd decorations
+        for (const island of config.palmTree) {
+            if (island === this.name) {
+                this.palm = new THREE.Mesh(geometry.palm, materials.colorset);
+                this.palm.position.set(this.position.x + (this.dockRadius / 4), 0, this.position.z - (this.dockRadius / 1.7));
+                this.palm.scale.x = this.dockRadius / 9;
+                this.palm.scale.y = this.dockRadius / 9;
+                this.palm.scale.z = this.dockRadius / 9;
+                scene.add(this.palm);
+            }
+        };
+        for (const island of config.christmasTree) {
+            if (island === this.name) {
+                this.christmasTree = models.elka;
+                this.christmasTree.position.set(this.position.x - (this.dockRadius / 10), (this.dockRadius / 50), this.position.z - (this.dockRadius / 10));
+                this.christmasTree.scale.x = this.dockRadius / (100 / 0.35);
+                this.christmasTree.scale.y = this.dockRadius / (100 / 0.35);
+                this.christmasTree.scale.z = this.dockRadius / (100 / 0.35);
+                scene.add(this.christmasTree);
+            }
+        };
+        for (const island of config.snowman) {
+            if (island === this.name) {
+                this.snowman = models.snowman;
+                this.snowman.position.set(this.position.x + (this.dockRadius / (10 / 3)), (this.dockRadius / (100 / 38)), this.position.z + (this.dockRadius / (100 / 35)));
+                this.snowman.rotation.set(0, -500, 0);
+                this.snowman.scale.x = this.dockRadius / (100 / 0.17);
+                this.snowman.scale.y = this.dockRadius / (100 / 0.17);
+                this.snowman.scale.z = this.dockRadius / (100 / 0.17);
+                scene.add(this.snowman);
+            }
+        };
     }
-}
+};
 
 Landmark.prototype.setName = function (name) {
     if (this.geometry !== undefined) {
