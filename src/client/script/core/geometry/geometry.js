13d05f53389cccf9b19c38c11700508161f6ae94
BR88C <bcroethel88@gmail.com>
2021-02-04 10:34:39 -0500

    change label colors

--- src/client/script/core/geometry/geometry.js
+++ src/client/script/core/geometry/geometry.js
@@ -85,10 +85,10 @@ let materials = {
 
 /* Set colors for text labels for player & boat names */
 let labelcolors = {
-    staff: new THREE.Color(0xbb15eb),
+    staff: new THREE.Color(0xd133ff),
     myself: new THREE.Color(0x00ff00),
     player: new THREE.Color(0xffffff),
-    captain: new THREE.Color(0xff0000)
+    captain: new THREE.Color(0xff7a38)
 };
 
 /* Create global vectors */
