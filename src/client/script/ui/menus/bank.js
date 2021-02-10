a21775fd65762661aa2d33f8598bd8179d278fa6
BR88C <bcroethel88@gmail.com>
2021-02-10 18:52:21 -0500

    fix setting total stored

--- src/client/script/ui/menus/bank.js
+++ src/client/script/ui/menus/bank.js
@@ -11,16 +11,19 @@ let setBankData = (data) => {
         $(`#bankContainer`).show();
         $(`#nabankContainer`).hide();
         $(`#my-deposits`).text(data.my);
-        if (data.total >= 1000 && data.total.toString().length <= 6) {
-            var goldTotalScore = `${Math.floor(data.total / 1000)} K`;
-            $(`#total-deposits`).text(goldTotalScore);
-        } else if (data.total.toString().length >= 7) {
-            goldTotalScore = `${Math.floor(data.total / 1000) / 1000} M`;
-            $(`#total-deposits`).text(goldTotalScore);
+        let goldTotalScore;
+        if (data.total) {
+            if (data.total >= 1000 && data.total.toString().length <= 6) {
+                goldTotalScore = `${Math.floor(data.total / 1000)} K`;
+            } else if (data.total.toString().length >= 7) {
+                goldTotalScore = `${Math.floor(data.total / 1000) / 1000} M`;
+            } else {
+                goldTotalScore = data.total;
+            }
         } else {
             goldTotalScore = data.total;
-            $(`#total-deposits`).text(goldTotalScore);
         }
+        $(`#total-deposits`).text(goldTotalScore);
         $(`#make-deposit`).attr({
             max: myPlayer.gold
         });
