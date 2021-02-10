9a8c9166799c9fa84f3b740672a825fabf1f8dd2
BR88C <bcroethel88@gmail.com>
2021-02-10 18:44:40 -0500

    replace with textFieldFocused

--- src/client/script/core/controls/keyboard.js
+++ src/client/script/core/controls/keyboard.js
@@ -29,15 +29,15 @@ let setUpKeyboard = () => {
             } else {
                 $(`#chat-message`).blur();
             }
-        } else if (evt.keyCode === 38 && !$(`#chat-message`).is(`:focus`) && !$(`#crew-name-edit-input`).is(`:focus`)) {
+        } else if (evt.keyCode === 38 && !textFieldFocused()) {
             keys_walkFwd = false;
-        } else if (evt.keyCode === 39 && !$(`#chat-message`).is(`:focus`) && !$(`#crew-name-edit-input`).is(`:focus`)) {
+        } else if (evt.keyCode === 39 && !textFieldFocused()) {
             keys_walkRight = false;
-        } else if (evt.keyCode === 40 && !$(`#chat-message`).is(`:focus`) && !$(`#crew-name-edit-input`).is(`:focus`)) {
+        } else if (evt.keyCode === 40 && !textFieldFocused()) {
             keys_walkBwd = false;
-        } else if (evt.keyCode === 37 && !$(`#chat-message`).is(`:focus`) && !$(`#crew-name-edit-input`).is(`:focus`)) {
+        } else if (evt.keyCode === 37 && !textFieldFocused()) {
             keys_walkLeft = false;
-        } else if (evt.keyCode === 16 && !$(`#chat-message`).is(`:focus`) && !$(`#crew-name-edit-input`).is(`:focus`)) {
+        } else if (evt.keyCode === 16 && !textFieldFocused()) {
             $(`#player-leaderboard`).hide();
         }
     };
@@ -50,44 +50,44 @@ let setUpKeyboard = () => {
  */
 document.onkeydown = function (evt) {
     evt = evt || window.event;
-    if (evt.keyCode === 38 && !$(`#chat-message`).is(`:focus`) && !$(`#clan-request`).is(`:focus`) && !$(`#crew-name-edit-input`).is(`:focus`)) { // Up arrow to move forward
+    if (evt.keyCode === 38 && !textFieldFocused()) { // Up arrow to move forward
         keys_walkFwd = true;
-    } else if (evt.keyCode === 39 && !$(`#chat-message`).is(`:focus`) && !$(`#clan-request`).is(`:focus`) && !$(`#crew-name-edit-input`).is(`:focus`)) { // Right arrow to move right
+    } else if (evt.keyCode === 39 && !textFieldFocused()) { // Right arrow to move right
         keys_walkRight = true;
-    } else if (evt.keyCode === 40 && !$(`#chat-message`).is(`:focus`) && !$(`#clan-request`).is(`:focus`) && !$(`#crew-name-edit-input`).is(`:focus`)) { // Down arrow to move backwards
+    } else if (evt.keyCode === 40 && !textFieldFocused()) { // Down arrow to move backwards
         keys_walkBwd = true;
-    } else if (evt.keyCode === 37 && !$(`#chat-message`).is(`:focus`) && !$(`#clan-request`).is(`:focus`) && !$(`#crew-name-edit-input`).is(`:focus`)) { // Left arrow to move left
+    } else if (evt.keyCode === 37 && !textFieldFocused()) { // Left arrow to move left
         keys_walkLeft = true;
-    } else if (!$(`#chat-message`).is(`:focus`) && !$(`#clan-request`).is(`:focus`) && (evt.keyCode === 49 || evt.keyCode === 97) && !$(`#crew-name-edit-input`).is(`:focus`)) {
+    } else if ((evt.keyCode === 49 || evt.keyCode === 97) && !textFieldFocused()) {
         if (myPlayer && myPlayer.geometry && myPlayer.activeWeapon !== 0) {
             socket.emit(`changeWeapon`, 0);
             myPlayer.isFishing = false;
         }
-    } else if (!$(`#chat-message`).is(`:focus`) && !$(`#clan-request`).is(`:focus`) && (evt.keyCode === 50 || evt.keyCode === 98) && !$(`#crew-name-edit-input`).is(`:focus`)) {
+    } else if ((evt.keyCode === 50 || evt.keyCode === 98) && !textFieldFocused()) {
         if (myPlayer && myPlayer.geometry && myPlayer.activeWeapon !== 1) {
             socket.emit(`changeWeapon`, 1);
             myPlayer.isFishing = false;
         }
-    } else if (!$(`#chat-message`).is(`:focus`) && !$(`#clan-request`).is(`:focus`) && (evt.keyCode === 51 || evt.keyCode === 99) && !$(`#crew-name-edit-input`).is(`:focus`)) {
+    } else if ((evt.keyCode === 51 || evt.keyCode === 99) && !textFieldFocused()) {
         if (myPlayer && myPlayer.geometry && myPlayer.activeWeapon !== 2) {
             socket.emit(`changeWeapon`, 2);
             myPlayer.isFishing = false;
         }
-    } else if (evt.keyCode === 77 && !$(`#chat-message`).is(`:focus`) && !$(`#clan-request`).is(`:focus`) && !$(`#crew-name-edit-input`).is(`:focus`)) {
+    } else if (evt.keyCode === 77 && !textFieldFocused()) {
         if (!$(`#minimap-container`).is(`:visible`)) {
             $(`#minimap-container`).show();
         } else {
             $(`#minimap-container`).hide();
         }
-    } else if (evt.keyCode === 16 && !$(`#chat-message`).is(`:focus`) && !$(`#clan-request`).is(`:focus`) && !$(`#crew-name-edit-input`).is(`:focus`)) {
+    } else if (evt.keyCode === 16 && !textFieldFocused()) {
         $(`#player-leaderboard`).show();
-    } else if (evt.keyCode === 81 && !$(`#chat-message`).is(`:focus`) && !$(`#clan-request`).is(`:focus`) && !$(`#crew-name-edit-input`).is(`:focus`)) {
+    } else if (evt.keyCode === 81 && !textFieldFocused()) {
         if (!$(`#quests-modal`).is(`:visible`)) {
             document.getElementById(`toggle-quest-button`).click();
         } else {
             $(`#quests-modal`).hide();
         }
-    } else if (evt.keyCode === 188 && !$(`#chat-message`).is(`:focus`) && !$(`#clan-request`).is(`:focus`) && !$(`#crew-name-edit-input`).is(`:focus`)) {
+    } else if (evt.keyCode === 188 && !textFieldFocused()) {
         if (!$(`#show-chat`).is(`:visible`)) {
             $(`#show-chat`).show();
             $(`#chat-div`).hide();
@@ -122,7 +122,7 @@ document.onkeydown = function (evt) {
     }
     // For experience points allocation
     else if (evt.keyCode >= 53 && evt.keyCode <= 55 &&
-        myPlayer && myPlayer.geometry && !$(`#chat-message`).is(`:focus`) && !$(`#clan-request`).is(`:focus`) && !$(`#make-deposit`).is(`:focus`) && !$(`#take-deposit`).is(`:focus`)) {
+        myPlayer && myPlayer.geometry && !textFieldFocused()) {
         let attribute = ExperiencePointsComponent.keys[evt.keyCode];
         ExperiencePointsComponent.clearStore().setStore((Store) => {
             Store.allocatedPoints[attribute] = 1;
