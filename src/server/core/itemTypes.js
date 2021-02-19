a9dd9b21ee109a2c2804f6a50a9a7e4176233b1c
BR88C <bcroethel88@gmail.com>
2021-02-18 23:16:34 -0500

    Update itemTypes.js

--- src/server/core/itemTypes.js
+++ src/server/core/itemTypes.js
@@ -1,190 +1,190 @@
 let itemTypes = [
-    /* {
-            id: 0,
-            name: `Cannon`,
-            Description: `Cannon for seadog`,
-            price: 500,
-            rarity: 1,
-            availableAt: []
-        },
-        {
-            id: 1,
-            name: `Fishing rod`,
-            Description: `Fishing rod for seadog`,
-            price: 500,
-            rarity: 1,
-            availableAt: []
-        }, */
-    {
-        id: 2,
-        name: `Sinker's Gloves`,
-        Description: `+25 cannon fire rate`,
-        price: 45e3,
-        rarity: 0.2,
-        availableAt: [`Spain`, `Brazil`],
-        attributes: {
-            attackSpeed: `25`
-        }
-    },
-    {
-        id: 3,
-        name: `Steel Barrel`,
-        Description: `+30 cannon distance`,
-        price: 35e3,
-        rarity: 0.6,
-        availableAt: [`Labrador`],
-        attributes: {
-            attackDistance: `30`
-        }
-    },
-    {
-        id: 4,
-        name: `Air Pegleg`,
-        Description: `+1 ship speed`,
-        price: 22e3,
-        rarity: 0.3,
-        availableAt: [`Jamaica`],
-        attributes: {
-            movementSpeed: `100`
-        }
-    },
-    {
-        id: 5,
-        name: `Blue Gunpowder`,
-        Description: `+8 cannon damage`,
-        price: 5e4,
-        rarity: 0.25,
-        availableAt: [`Jamaica`],
-        attributes: {
-            attackDamage: `8`
-        }
-    },
-    {
-        id: 6,
-        name: `Cannon distance upgrade`,
-        Description: `+5 cannon distance`,
-        price: 4e3,
-        rarity: 1,
-        attributes: {
-            attackDistance: `5`
-        }
-    },
-    {
-        id: 7,
-        name: `Attack speed upgrade`,
-        Description: `+5 cannon fire rate`,
-        price: 2e3,
-        rarity: 1,
-        attributes: {
-            attackSpeed: `5`
-        }
-    },
-    {
-        id: 8,
-        name: `Damage upgrade`,
-        Description: `+5 cannon damage`,
-        price: 5e3,
-        rarity: 1,
-        attributes: {
-            attackDamage: `5`
-        }
-    },
-    {
-        id: 9,
-        name: `Ship speed upgrade`,
-        Description: `+0.2 ship speed`,
-        price: 3e3,
-        rarity: 1,
-        attributes: {
-            movementSpeed: `20`
-        }
-    },
-    {
-        id: 10,
-        name: `Bruiser`,
-        Description: `+2 cannon damage<br/>+10 cannon fire rate`,
-        price: 2e4,
-        rarity: 0.35,
-        availableAt: [`Spain, Brazil`],
-        attributes: {
-            attackSpeed: `10`,
-            attackDamage: `2`
-        }
-    },
-    {
-        id: 11,
-        name: `Demolisher`,
-        Description: `+10 cannon damage<br/>+30 cannon fire rate<br/>Requirements<br/> - Sink 10 ships<br/> - Trade goods worth 100,000 gold`,
-        price: 1e5,
-        rarity: 1,
-        availableAt: [`Jamaica`],
-        attributes: {
-            attackSpeed: `30`,
-            attackDamage: `10`
-        }
-    },
-    {
-        id: 12,
-        name: `Drifter`,
-        Description: `+25 cannon damage<br/>+1.5 ship speed`,
-        price: 4e5,
-        rarity: 0.45,
-        availableAt: [`Guinea`, `Labrador`],
-        attributes: {
-            attackDamage: `25`,
-            movementSpeed: `150`
-        }
-    },
-    {
-        id: 13,
-        name: `Reinforced Planks`,
-        Description: `25% damage reduction`,
-        price: 35e3,
-        rarity: 0.45,
-        availableAt: [`Brazil`],
-        attributes: {
-            armor: `25`
-        }
-    },
-    {
-        id: 14,
-        name: `Fountain of Youth`,
-        Description: `Reset your skill points and allow them to be reallocated.<br/>Can only be bought once.`,
-        price: 15e4,
-        rarity: 0.33,
-        availableAt: [`Jamaica`]
-    },
-    {
-        id: 15,
-        name: `Nitro Peg`,
-        Description: `+2 ship speed`,
-        price: 2e5,
-        rarity: 0.2,
-        availableAt: [`Jamaica`],
-        attributes: {
-            movementSpeed: `200`
-        }
-    },
-    {
-        id: 16,
-        name: `Advanced Toolkit`,
-        Description: `+8 Regen`,
-        price: 2e5,
-        rarity: 0.2,
-        availableAt: [`Malaysia`],
-        attributes: {
-            regen: `8`
-        }
-    },
-    {
-        id: 17,
-        name: `Steel Reinforced Planks`,
-        Description: `50% damage reduction`,
-        price: 2e5,
-        rarity: 0.3,
-        availableAt: [`Taiwan`],
-        attributes: {
-            armor: `50`
-        }
-    }
+    // /* {
+    //         id: 0,
+    //         name: `Cannon`,
+    //         Description: `Cannon for seadog`,
+    //         price: 500,
+    //         rarity: 1,
+    //         availableAt: []
+    //     },
+    //     {
+    //         id: 1,
+    //         name: `Fishing rod`,
+    //         Description: `Fishing rod for seadog`,
+    //         price: 500,
+    //         rarity: 1,
+    //         availableAt: []
+    //     }, */
+    // {
+    //     id: 2,
+    //     name: `Sinker's Gloves`,
+    //     Description: `+25 cannon fire rate`,
+    //     price: 45e3,
+    //     rarity: 0.2,
+    //     availableAt: [`Spain`, `Brazil`],
+    //     attributes: {
+    //         attackSpeed: `25`
+    //     }
+    // },
+    // {
+    //     id: 3,
+    //     name: `Steel Barrel`,
+    //     Description: `+30 cannon distance`,
+    //     price: 35e3,
+    //     rarity: 0.6,
+    //     availableAt: [`Labrador`],
+    //     attributes: {
+    //         attackDistance: `30`
+    //     }
+    // },
+    // {
+    //     id: 4,
+    //     name: `Air Pegleg`,
+    //     Description: `+1 ship speed`,
+    //     price: 22e3,
+    //     rarity: 0.3,
+    //     availableAt: [`Jamaica`],
+    //     attributes: {
+    //         movementSpeed: `100`
+    //     }
+    // },
+    // {
+    //     id: 5,
+    //     name: `Blue Gunpowder`,
+    //     Description: `+8 cannon damage`,
+    //     price: 5e4,
+    //     rarity: 0.25,
+    //     availableAt: [`Jamaica`],
+    //     attributes: {
+    //         attackDamage: `8`
+    //     }
+    // },
+    // {
+    //     id: 6,
+    //     name: `Cannon distance upgrade`,
+    //     Description: `+5 cannon distance`,
+    //     price: 4e3,
+    //     rarity: 1,
+    //     attributes: {
+    //         attackDistance: `5`
+    //     }
+    // },
+    // {
+    //     id: 7,
+    //     name: `Attack speed upgrade`,
+    //     Description: `+5 cannon fire rate`,
+    //     price: 2e3,
+    //     rarity: 1,
+    //     attributes: {
+    //         attackSpeed: `5`
+    //     }
+    // },
+    // {
+    //     id: 8,
+    //     name: `Damage upgrade`,
+    //     Description: `+5 cannon damage`,
+    //     price: 5e3,
+    //     rarity: 1,
+    //     attributes: {
+    //         attackDamage: `5`
+    //     }
+    // },
+    // {
+    //     id: 9,
+    //     name: `Ship speed upgrade`,
+    //     Description: `+0.2 ship speed`,
+    //     price: 3e3,
+    //     rarity: 1,
+    //     attributes: {
+    //         movementSpeed: `20`
+    //     }
+    // },
+    // {
+    //     id: 10,
+    //     name: `Bruiser`,
+    //     Description: `+2 cannon damage<br/>+10 cannon fire rate`,
+    //     price: 2e4,
+    //     rarity: 0.35,
+    //     availableAt: [`Spain, Brazil`],
+    //     attributes: {
+    //         attackSpeed: `10`,
+    //         attackDamage: `2`
+    //     }
+    // },
+    // {
+    //     id: 11,
+    //     name: `Demolisher`,
+    //     Description: `+10 cannon damage<br/>+30 cannon fire rate<br/>Requirements<br/> - Sink 10 ships<br/> - Trade goods worth 100,000 gold`,
+    //     price: 1e5,
+    //     rarity: 1,
+    //     availableAt: [`Jamaica`],
+    //     attributes: {
+    //         attackSpeed: `30`,
+    //         attackDamage: `10`
+    //     }
+    // },
+    // {
+    //     id: 12,
+    //     name: `Drifter`,
+    //     Description: `+25 cannon damage<br/>+1.5 ship speed`,
+    //     price: 4e5,
+    //     rarity: 0.45,
+    //     availableAt: [`Guinea`, `Labrador`],
+    //     attributes: {
+    //         attackDamage: `25`,
+    //         movementSpeed: `150`
+    //     }
+    // },
+    // {
+    //     id: 13,
+    //     name: `Reinforced Planks`,
+    //     Description: `25% damage reduction`,
+    //     price: 35e3,
+    //     rarity: 0.45,
+    //     availableAt: [`Brazil`],
+    //     attributes: {
+    //         armor: `25`
+    //     }
+    // },
+    // {
+    //     id: 14,
+    //     name: `Fountain of Youth`,
+    //     Description: `Reset your skill points and allow them to be reallocated.<br/>Can only be bought once.`,
+    //     price: 15e4,
+    //     rarity: 0.33,
+    //     availableAt: [`Jamaica`]
+    // },
+    // {
+    //     id: 15,
+    //     name: `Nitro Peg`,
+    //     Description: `+2 ship speed`,
+    //     price: 2e5,
+    //     rarity: 0.2,
+    //     availableAt: [`Jamaica`],
+    //     attributes: {
+    //         movementSpeed: `200`
+    //     }
+    // },
+    // {
+    //     id: 16,
+    //     name: `Advanced Toolkit`,
+    //     Description: `+8 Regen`,
+    //     price: 2e5,
+    //     rarity: 0.2,
+    //     availableAt: [`Malaysia`],
+    //     attributes: {
+    //         regen: `8`
+    //     }
+    // },
+    // {
+    //     id: 17,
+    //     name: `Steel Reinforced Planks`,
+    //     Description: `50% damage reduction`,
+    //     price: 2e5,
+    //     rarity: 0.3,
+    //     availableAt: [`Taiwan`],
+    //     attributes: {
+    //         armor: `50`
+    //     }
+    // }
 ];
