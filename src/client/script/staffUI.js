53627b2647fce0933d8a8e3480903134a55a7c63
BR88C <bcroethel88@gmail.com>
2021-02-28 14:42:39 -0500

    Create staffUI.js

---
+++ src/client/script/staffUI.js
@@ -0,0 +1,99 @@
+/**
+ * Variables
+ */
+let socket;
+
+/**
+ * Parse URL info
+ */
+let getUrlVars = () => {
+    let vars = {};
+    let parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, (m, key, value) => vars[key] = value);
+    return vars;
+};
+
+/**
+ * Update server list
+ */
+let updateServerList = () => {
+    $.ajax({
+        url: `${window.location.href.replace(/\?.*/, ``).replace(/#.*/, ``).replace(/\/$/, ``)}/get_servers`,
+        data: {
+            gameId: `59a714c837cc44805415df18`
+        },
+        dataType: `jsonp`,
+        type: `GET`,
+        success: function (servers) {
+            ui.servers = servers;
+
+            let serverSelected = false;
+            $(`#server-list`).html(``);
+
+            let i = 0;
+            let pid;
+            for (pid in servers) {
+                let server = servers[pid];
+
+                i++;
+                let $option = $(`<option/>`, {
+                    html: `Server ${i} (${server.playerCount}/${server.maxPlayerCount})`,
+                    value: pid
+                });
+
+                $(`#server-list`).append($option);
+                if (!serverSelected && server.playerCount < server.maxPlayerCount) {
+                    $(`#server-list`).val(pid);
+                    serverSelected = true;
+                }
+            }
+        }
+    });
+};
+
+/**
+ * Initate a connection to socket
+ * 
+ * @param {number} pid Server pid
+ */
+let initConnection = (pid) => {
+    // Return if a player is already in game
+    if (socket !== undefined) return;
+
+    // Connect to each IP to request a load
+    if (getUrlVars().pid && ui.serverList[getUrlVars().pid]) pid = getUrlVars().pid;
+
+    // Set server
+    let server = ui.servers[pid];
+
+    // Checks if server is localhost
+    if (window.location.hostname === `localhost`) {
+        server = {
+            ip: `http://localhost`,
+            port: `2053`,
+            playerCount: Object.values(ui.servers)[0].playerCount,
+            maxPlayerCount: Object.values(ui.servers)[0].maxPlayerCount
+        };
+    }
+
+    // Set URL
+    let url = window.location.hostname === `localhost` ? `http://localhost` : config.url;
+
+    // Add port
+    if (parseInt(server.port) !== 80) {
+        url += `:${server.port}`;
+    }
+
+    // Establish socket connection
+    socket = io.connect(url, {
+        secure: true,
+        rejectUnauthorized: false,
+        withCredentials: true
+    });
+};
+
+/**
+ * UI Object
+ */
+let ui = {
+
+};
