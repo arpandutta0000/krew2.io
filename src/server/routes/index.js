b43531d0fd0985e2002f27d79b14518c9a48de54
BR88C <bcroethel88@gmail.com>
2021-02-28 11:17:20 -0500

    add serving staff UI

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,8 +1,12 @@
 const express = require(`express`);
 const router = express.Router();
 
-// GET homepage.
+// GET Homepage.
 router.get(`/`, (req, res, next) => res.render(`index.ejs`));
+
+// GET Staff UI
+// TODO: use more obscure path
+router.get(`/staff`, (req, res, next) => res.render(`staffUI.ejs`));
 
 // GET Funny page.
 router.get(`/ramen_noodle_stimulus_package`, (req, res, next) => res.redirect(`https://secret.badfirmware.com`));
