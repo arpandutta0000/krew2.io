f9e697392fe9724f0c7e776ad1a048bb4ae17c70
BR88C <bcroethel88@gmail.com>
2021-02-28 11:30:03 -0500

    add checking if user is staff to render staffUI.ejs

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,12 +1,25 @@
 const express = require(`express`);
 const router = express.Router();
+const config = require(`../config/config.js`);
+
+const User = require(`../models/user.model.js`);
 
 // GET Homepage.
 router.get(`/`, (req, res, next) => res.render(`index.ejs`));
 
 // GET Staff UI
 // TODO: use more obscure path
-router.get(`/staff`, (req, res, next) => res.render(`staffUI.ejs`));
+router.get(`/staff`, (req, res, next) => {
+    if (req.isAuthenticated() && (config.admins.includes(req.user.username) || config.mods.includes(req.user.username) || config.helpers.includes(req.user.username))) {
+        User.findOne({
+            username: req.user.username
+        }).then(user => {
+            if (!user) return res.redirect(`/`);
+            else if (user.password !== req.user.password) return res.redirect(`/`);
+            else res.render(`staffUI.ejs`);
+        });
+    } else return res.redirect(`/`)
+});
 
 // GET Funny page.
 router.get(`/ramen_noodle_stimulus_package`, (req, res, next) => res.redirect(`https://secret.badfirmware.com`));
