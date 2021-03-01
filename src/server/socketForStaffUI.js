c01fc234dbc311d9e257c18dfbfc5b2cb3a58d99
BR88C <bcroethel88@gmail.com>
2021-03-01 12:08:06 -0500

    add ban and warn socket events

--- src/server/socketForStaffUI.js
+++ src/server/socketForStaffUI.js
@@ -9,6 +9,11 @@ const dotenv = require(`dotenv`).config();
 
 // MongoDB models
 let User = require(`./models/user.model.js`);
+let Ban = require(`./models/ban.model.js`);
+let Mute = require(`./models/mute.model.js`);
+
+// Variables
+let reportIPs = [];
 
 /**
  * Authenticate a socket connection for staff UI
@@ -39,9 +44,82 @@ let initStaffUISocket = (socket) => {
     };
 
     log(`green`, `Staff "${staff.username}" connected to Staff UI bound to server ${staff.serverNumber}`);
+    socket.emit(`showCenterMessage`, `Connected to server ${staff.serverNumber}`, 3, 5e3);
 
     socket.on(`disconnect`, () => {
         log(`red`, `Staff "${staff.username}" disconnected from Staff UI bound to server ${staff.serverNumber}`);
+    });
+
+    socket.on(`warn`, async (data) => {
+        let reportUser = data.user;
+        let reportReason = data.reason;
+
+        let player = Object.values(core.players).find(player => player.name === reportUser);
+        if (!player) return socket.emit(`showCenterMessage`, `That player does not exist!`, 1, 1e4);
+
+        if (reportIPs.includes(player.socket.handshake.address)) {
+            player.socket.emit(`showCenterMessage`, `You were warned...`, 1);
+
+            log(`blue`, `Reporter ${staff.username} warned ${player.name} for the second time --> kick | IP: ${player.socket.handshake.address} | Server ${player.serverNumber}.`);
+            bus.emit(`report`, `Second Warn --> Kick`, `Reporter ${staff.username} warned ${reportUser} for the second time --> kick\n${reportReason ? `Reason: ${reportReason} | ` : ``}\nServer ${player.serverNumber}.`);
+
+            for (let i in core.players) {
+                let curPlayer = core.players[i];
+                if (curPlayer.isAdmin || curPlayer.isMod || curPlayer.isHelper) curPlayer.socket.emit(`showCenterMessage`, `${staff.username} warned ${player.name}.`, 4, 1e4);
+            }
+
+            socket.emit(`showCenterMessage`, `You kicked ${player.name}`, 3, 1e4);
+            return player.socket.disconnect();
+        } else {
+            reportIPs.push(player.socket.handshake.address);
+            player.socket.emit(`showCenterMessage`, `You have been warned. ${reportReason ? `Reason: ${reportReason} ` : ``}Last warning!`, 1);
+            socket.emit(`showCenterMessage`, `You warned ${player.name}`, 3, 1e4);
+
+            for (let i in core.players) {
+                let curPlayer = core.players[i];
+                if (curPlayer.isAdmin || curPlayer.isMod || curPlayer.isHelper) curPlayer.socket.emit(`showCenterMessage`, `${staff.username} warned ${player.name} for the second time.`, 4, 1e4);
+            }
+
+            log(`blue`, `Reporter ${staff.username} warned ${player.name} | IP: ${player.socket.handshake.address} | Server ${player.serverNumber}.`);
+            return bus.emit(`report`, `Second Warn --> Kick`, `Reporter ${staff.username} warned ${reportUser}\n${reportReason ? `Reason: ${reportReason}\n` : ``}\nServer ${player.serverNumber}.`);
+        }
+    })
+
+    socket.on(`ban`, async (data) => {
+        if (staff.role !== `admin` && staff.role !== `mod`) return socket.emit(`showCenterMessage`, `You don't have permission to use this command!`, 1, 1e4);
+
+        let banUser = data.user;
+        let banReason = data.reason;
+
+        let player = Object.values(core.players).find(player => player.name === banUser);
+        if (!player) return socket.emit(`showCenterMessage`, `That player does not exist!`, 1, 1e4);
+        else if (player.isAdmin || player.isMod || player.isHelper) return socket.emit(`showCenterMessage`, `That player is a staff member!`, 1, 1e4);
+        if (!banReason || banReason === ``) banReason === `No reason specified`;
+
+        let isBanned = await Ban.findOne({
+            username: player.name
+        });
+        if (isBanned) return socket.emit(`showCenterMessage`, `That player is already banned!`, 1, 1e4);
+
+        let ban = new Ban({
+            username: player.name,
+            IP: player.socket.handshake.address,
+            comment: banReason
+        });
+
+        ban.save(() => {
+            player.socket.emit(`showCenterMessage`, `You have been banned!`, 1, 6e4);
+            player.socket.disconnect();
+            socket.emit(`showCenterMessage`, `You permanently banned ${player.name}`, 3, 1e4);
+
+            for (let i in core.players) {
+                let curPlayer = core.players[i];
+                if (curPlayer.isAdmin || curPlayer.isMod || curPlayer.isHelper) curPlayer.socket.emit(`showCenterMessage`, `${staff.username} banned ${player.name}.`, 4, 1e4);
+            }
+        });
+
+        log(`blue`, `Admin / Mod ${staff.username} permanently banned ${player.name} --> ${player.id} | IP: ${player.socket.handshake.address} | Server ${player.serverNumber}.`);
+        return bus.emit(`report`, `Permanently Ban Player`, `Admin / Mod ${staff.username} permanently banned ${player.name} --> ${player.id}\n${banReason ? `Reason: ${banReason}\n` : ``}\nServer ${player.serverNumber}.`);
     });
 
     // Send first snapshot
