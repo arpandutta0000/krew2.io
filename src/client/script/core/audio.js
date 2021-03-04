044a943ca18e264fdf5543329bda018eedad7cb3
BR88C <bcroethel88@gmail.com>
2021-03-03 22:20:44 -0500

    remove isMusic

--- src/client/script/core/audio.js
+++ src/client/script/core/audio.js
@@ -16,9 +16,8 @@ let updateMusic = () => {
  * @param {boolean} stack If the audio file should be able to be stacked
  * @param {number} volume Volume multiplier - 1 = Normal volume
  * @param {string} fileId The file ID for the audio file
- * @param {boolean} isMusic If the file is music
  */
-let playAudioFile = (loop, stack, volume, fileId, isMusic) => {
+let playAudioFile = (loop, stack, volume, fileId) => {
     const musicValue = document.getElementById(`music-control`);
     const sfxValue = document.getElementById(`sfx-control`);
 
@@ -28,11 +27,10 @@ let playAudioFile = (loop, stack, volume, fileId, isMusic) => {
 
         let copy = audio.cloneNode(true);
         copy.volume = loop ? volume * 0.1 * musicValue.value / musicValue.max : volume * 0.35 * sfxValue.value / sfxValue.max;
-        console.log(copy.volume)
         copy.play();
     } else {
-        document.getElementById(fileId).play();
         document.getElementById(fileId).volume = loop ? volume * 0.1 * musicValue.value / musicValue.max : volume * 0.45 * sfxValue.value / sfxValue.max;
+        document.getElementById(fileId).play();
     }
 };
 
@@ -50,13 +48,12 @@ let stopAudioFile = (fileId) => document.getElementById(fileId).pause();
  * @param {number} endVolume The final volume after fading in
  * @param {number} time The length of time to fade in for in milliseconds
  * @param {string} fileId The file ID for the audio file
- * @param {boolean} isMusic If the file is music
  */
-let fadeInAudio = (loop, endVolume, time, fileId, isMusic) => {
+let fadeInAudio = (loop, endVolume, time, fileId) => {
     const musicValue = document.getElementById(`music-control`);
     const sfxValue = document.getElementById(`sfx-control`);
 
-    playAudioFile(loop, false, 0, fileId, isMusic);
+    playAudioFile(loop, false, 0, fileId);
 
     $(`#${fileId}`).animate({
         volume: loop ? endVolume * 0.1 * musicValue.value / musicValue.max : endVolume * 0.35 * sfxValue.value / sfxValue.max
