692a6e750373b00a9e5ead9525dd3b8f6d3fc962
BR88C <bcroethel88@gmail.com>
2021-03-05 20:59:36 -0500

    JSDoc typings

--- src/server/core/core.js
+++ src/server/core/core.js
@@ -1,6 +1,20 @@
+/* Import Entity class for JSDoc typing */
+const Entity = require(`./entities/Entity`);
+
+/**
+ * Entities array. Includes all entity objects
+ * 
+ * @type {Entity[]}
+ */
 const entities = [];
 
-const iterateEntities = dt => {
+/**
+ * Iterate through every entity and run logic
+ * 
+ * @param {number} dt DT
+ * @returns {void}
+ */
+const iterateEntities = (dt) => {
     // Tick each entity.
     for (const entity of entities) entity.tick(dt);
 };
--- src/server/core/utils.js
+++ src/server/core/utils.js
@@ -1,147 +1,151 @@
-const { entities } = require(`./core.js`);
+/* Import entities */
+const {
+    entities
+} = require(`./core.js`);
 
-let lerp = (start, end, amount) => (1 - amount) * start + amount * end;
-
-let charLimit = function (text, chars, suffix) {
-    chars = chars || 140;
-    suffix = suffix || ``;
-    text = (`${text}`).replace(/(\t|\n)/gi, ``).replace(/\s\s/gi, ` `);
-    if (text.length > chars) {
-        return text.slice(0, chars - suffix.length).replace(/(\.|\,|:|-)?\s?\w+\s?(\.|\,|:|-)?$/, suffix);
-    }
-
-    return text;
-};
-
-let distance = (x1, x2, y1, y2) => {
-    const deltaX = x2 - x1;
-    const deltaY = y2 - y1;
-
-    return Math.sqrt(deltaX ^ 2 + deltaY ^ 2);
-};
-
-let worldAngle = vector => {
-    let result = vector.angle() + Math.PI * 0.5;
-    if (result > Math.PI * 2) {
-        result -= Math.PI * 2;
-    }
-
-    result = Math.PI * 2 - result;
-    return result;
-};
-
-let anglediff = (firstAngle, secondAngle) => {
-    let difference = secondAngle - firstAngle;
-    while (difference < -Math.PI) {
-        difference += Math.PI * 2.0;
-    }
-
-    while (difference > Math.PI) {
-        difference -= Math.PI * 2.0;
-    }
-
-    return difference;
-};
-
-const angleToVector = (angle) => new THREE.Vector2(-Math.sin(angle), -Math.cos(angle));
-
-const rotationToPosition = (origin, target) => worldAngle(new THREE.Vector2(target.x - origin.x, target.z - origin.z));
-
-const rotationToObject = (origin, target) => worldAngle(new THREE.Vector2(target.position.x - origin.position.x, target.position.z - origin.position.z));
-
-const distanceToPosition = (origin, target) => origin.position.distanceTo(target);
-
-const distanceToPositionSquared = (origin, target) => origin.position.distanceToSquared(target);
-
-const distanceToObject = (origin, target) => origin.position.distanceTo(target.position);
-
-const distanceToObjectSquared = (origin, target) => origin.position.distanceToSquared(target.position);
-
-/**
- * This method checks if a 3d object is in the players vision range
- * Is created with a factory function to create the frustum only once and
- * not on every check
- * @return {Boolean}
- */
-let inPlayersVision = (function () {
-    let frustum = new THREE.Frustum();
+module.exports = {
     /**
-     * This is the exported function that will used for the check
-     * @param  {Object} object3d    It must be a 3d object with a position property
-     * @param  {Object} camera      It must be the camera to compare with
-     * @return {Boolean}            Returns true if the player sees the object or false on the contrary
+     * Method to perform linear interpolation
+     * 
+     * @param {number} start Starting value
+     * @param {number} end Ending value
+     * @param {number} amount lerp amount
+     * @returns {number} lerp output
      */
-    let inPlayersVision = function (object3d, camera) {
-        // If the object has no position property just return false
-        if (object3d.position === undefined) {
-            return false;
-        }
+    lerp: (start, end, amount) => (1 - amount) * start + amount * end,
 
-        camera.updateMatrix();
-        camera.updateMatrixWorld();
+    /**
+     * Returns a string cut at a character limit
+     * 
+     * @param {string} text Input string
+     * @param {?number} chars Amount of allowed characters (Defaults to 140)
+     * @param {?string} suffix Suffix to be preserved
+     * @returns {string} Output string
+     */
+    charLimit: (text, chars, suffix) => {
+        chars = chars || 140;
+        suffix = suffix || ``;
+        text = (`${text}`).replace(/(\t|\n)/gi, ``).replace(/\s\s/gi, ` `);
+        if (text.length > chars) return text.slice(0, chars - suffix.length).replace(/(\.|\,|:|-)?\s?\w+\s?(\.|\,|:|-)?$/, suffix);
 
-        frustum.setFromMatrix(
-            new THREE.Matrix4()
+        return text;
+    },
+
+    /**
+     * Calculate the distance between 2 positions
+     * 
+     * @param {{ x: number, z: number }} p1 Position 1
+     * @param {{ x: number, z: number }} p2 Position 2
+     * @returns {number} Distance between positions
+     */
+    distance: (p1, p2) => {
+        let dx = p2.x - p1.x;
+        let dz = p2.z - p1.z;
+        return Math.sqrt(dx * dx + dz * dz);
+    },
+
+    /**
+     * Calculate the world angle of a vector
+     * 
+     * @param {object} vector Vector object
+     * @returns {number} Angle
+     */
+    worldAngle: (vector) => {
+        let result = vector.angle() + Math.PI * 0.5;
+        if (result > Math.PI * 2) result -= Math.PI * 2;
+        result = Math.PI * 2 - result;
+        return result;
+    },
+
+    /**
+     * Calculates the difference between angles
+     * 
+     * @param {number} firstAngle First angle
+     * @param {number} secondAngle Second angle
+     * @returns {number} Difference
+     */
+    anglediff: (firstAngle, secondAngle) => {
+        let difference = secondAngle - firstAngle;
+        while (difference < -Math.PI) difference += Math.PI * 2.0;
+
+        while (difference > Math.PI) difference -= Math.PI * 2.0;
+
+        return difference;
+    },
+
+    /**
+     * Convert an angle to a Vector 2
+     * 
+     * @param {number} angle Angle
+     * @returns {object} THREE.Vector2 resultant
+     */
+    angleToVector: (angle) => new THREE.Vector2(-Math.sin(angle), -Math.cos(angle)),
+
+    /**
+     * Finds the rotation to an object
+     * 
+     * @param {{ x: number, z: number }} origin Origin
+     * @param {{ x: number, z: number }} target Target
+     * @returns {number} Angle
+     */
+    rotationToObject: (origin, target) => this.worldAngle(new THREE.Vector2(target.position.x - origin.position.x, target.position.z - origin.position.z)),
+
+    /**
+     * This method checks if a 3d object is in the players vision range
+     * Is created with a factory function to create the frustum only once and
+     * not on every check
+     * @returns {boolean}
+     */
+    inPlayersVision: (function () {
+        let frustum = new THREE.Frustum();
+        /**
+         * This is the exported function that will used for the check
+         * @param  {object} object3d    It must be a 3d object with a position property
+         * @param  {object} camera      It must be the camera to compare with
+         * @returns {boolean}            Returns true if the player sees the object or false on the contrary
+         */
+        let inPlayersVision = function (object3d, camera) {
+            // If the object has no position property just return false
+            if (object3d.position === undefined) {
+                return false;
+            }
+
+            camera.updateMatrix();
+            camera.updateMatrixWorld();
+
+            frustum.setFromMatrix(
+                new THREE.Matrix4()
                 .multiplyMatrices(
                     camera.projectionMatrix,
                     camera.matrixWorldInverse
                 )
-        );
+            );
 
-        // Return if the object is in the frustum
-        return frustum.containsPoint(object3d.position);
-    };
+            // Return if the object is in the frustum
+            return frustum.containsPoint(object3d.position);
+        };
 
-    // Returns the final function
-    return inPlayersVision;
-})();
+        // Returns the final function
+        return inPlayersVision;
+    })(),
 
-let getFixedFrameRateMethod = (fps, callback) => {
-    fps = fps || 5;
-    let time = performance.now();
-    let previousTime = performance.now();
-    let method = function () {
-        time = performance.now();
-        if (time - previousTime > 1000 / fps) {
-            previousTime = time;
-            if (typeof callback === `function`) {
-                requestAnimationFrame(callback.bind(this));
-            }
-        }
-    };
+    /**
+     * Generates a random entity ID
+     * 
+     * @returns {string} Entity ID
+     */
+    randomID: () => {
+        let id = ``;
+        while (entities.find(entity => entity.id === id)) id = Math.random().toString(36).substring(6, 10);
+        return id;
+    },
 
-    return method;
-};
-
-const randomID = () => {
-    let id = ``;
-    while (entities.find(entity => entity.id === id)) id = Math.random().toString(36).substring(6, 10);
-};
-
-const randomInt = (min, max) => (Math.floor(Math.random() * (max - min)) + min) * 1000;
-
-module.exports = {
-    lerp,
-    charLimit,
-
-    entityDistance,
-    distance,
-
-    anglediff,
-    angleToVector,
-
-    rotationToPosition,
-    rotationToObject,
-
-    distanceToPosition,
-    distanceToPositionSquared,
-
-    distanceToObject,
-    distanceToObjectSquared,
-
-    inPlayersVision,
-    getFixedFrameRateMethod,
-
-    randomID,
-    randomInt
+    /**
+     * Generates a random integer between 2 bounds
+     * 
+     * @param {number} min Minimum integer
+     * @param {number} max Maximum integer
+     * @returns {number} Random integer
+     */
+    randomInt: (min, max) => (Math.floor(Math.random() * (max - min)) + min) * 1000
 };
