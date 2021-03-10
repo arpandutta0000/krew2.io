03323bb289aef5f9fa65b881ea15c0554bcc4327
Itsdabombgit <57869163+Itsdabombgit@users.noreply.github.com>
2021-03-10 12:17:56 +0700

    hm

--- src/server/config/thugConfig.js
+++ src/server/config/thugConfig.js
@@ -4,67 +4,67 @@ module.exports = {
             name: `devclied`,
             hash: `85278e7a17c0f9d3af915746df605017`
         },
-        {
-            name: `BR88C`,
-            hash: `97ddaf7ed95d3753fb34a2c85a13cd1b`
-        },
+        //{
+        //    name: `BR88C`,
+        //    hash: `97ddaf7ed95d3753fb34a2c85a13cd1b`
+        //},
         {
             name: `BombiBomba`,
             hash: ``
-        },
-        {
-            name: `DamienVesper`,
-            hash: `c9c0066ba38a78db4c7d3f6c2cec51752`
-        },
-        {
-            name: `LeoLeoLeo`,
-            hash: `d9e1aafc0229b8f4380a1f5fdb9dc999`
-        },
-        {
-            name: `Sjmun`,
-            hash: `ccd4fce3517aae9a4b0ba119c092fe2b`
-        },
-        {
-            name: `Speedy_Sloth`,
-            hash: `d44f081ab871f64193688aa973480807`
         }
+        //{
+        //    name: `DamienVesper`,
+        //    hash: `c9c0066ba38a78db4c7d3f6c2cec51752`
+        //},
+        //{
+        //    name: `LeoLeoLeo`,
+        //    hash: `d9e1aafc0229b8f4380a1f5fdb9dc999`
+        //},
+        //{
+        //    name: `Sjmun`,
+        //    hash: `ccd4fce3517aae9a4b0ba119c092fe2b`
+        //},
+        //{
+        //    name: `Speedy_Sloth`,
+        //    hash: `d44f081ab871f64193688aa973480807`
+        //}
     ],
     Mods: [
-        {
-            name: `Fiftyyyyy`,
-            hash: `6375f9fa6da480249bb2ad20a7ff04cb`
-        },
-        {
-            name: `TheChoco`,
-            hash: `ccd4fce3517aae9a4b0ba119c092fe2b`
-        },
-        {
-            name: `Kekmw`,
-            hash: `ccd4fce3517aae9a4b0ba119c092fe2b`
-        }
+        //{
+        //    name: `Fiftyyyyy`,
+        //    hash: `6375f9fa6da480249bb2ad20a7ff04cb`
+        //},
+        //{
+        //    name: `TheChoco`,
+        //    hash: `ccd4fce3517aae9a4b0ba119c092fe2b`
+        //},
+        //{
+        //    name: `Kekmw`,
+        //    hash: `ccd4fce3517aae9a4b0ba119c092fe2b`
+        //}
     ],
     Helpers: [
-        {
-            name: `Tommy_Finle`,
-            hash: ``
-        },
-        {
-            name: `ObamaKindaCare`,
-            hash: ``
-        },
-        {
-            name: `Sky920`,
-            hash: ``
-        }
+        //{
+        //    name: `Tommy_Finle`,
+        //    hash: ``
+        //},
+        //{
+        //    name: `ObamaKindaCare`,
+        //    hash: ``
+        //},
+        //{
+        //    name: `Sky920`,
+        //    hash: ``
+        //}
     ],
     Designers: [
-        {
-            name: `bread`,
-            hash: ``
-        },
-        {
-            name: `ward0g71`,
-            hash: ``
-        }
+        //{
+        //    name: `bread`,
+        //    hash: ``
+        //},
+        //{
+        //    name: `ward0g71`,
+        //    hash: ``
+       // }
     ]
 };
