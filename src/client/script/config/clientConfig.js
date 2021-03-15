68cabdcaccff851a29c9144181803ce027ab41d6
LapaMauve <contact@lapamauve.io>
2021-03-15 20:17:22 +0000

    :bug: wrong domain

--- src/client/script/config/clientConfig.js
+++ src/client/script/config/clientConfig.js
@@ -6,7 +6,7 @@ const config = {
     Designers: [`bread`, `ward0g71`],
 
     // Set URL
-    url: `http://45.63.5.201`,
+    url: `https://krew.io`,
 
     // Set config.worldsize
     worldsize: 2500,
