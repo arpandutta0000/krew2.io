1db93dfac692efd8ed3310cedade4bae89e7ab8c
Damien Vesper <ldamienvesper@gmail.com>
2021-03-20 14:28:26 -0500

    FUNNY

--- src/server/core/itemTypes.js
+++ src/server/core/itemTypes.js
@@ -3,7 +3,7 @@ let itemTypes = [
         id: 0,
         name: `Attack speed upgrade`,
         Description: `+5 cannon fire rate`,
-        price: 2e3,
+        price: 0, // 2e3,
         rarity: 1,
         attributes: {
             attackSpeed: `5`
@@ -13,7 +13,7 @@ let itemTypes = [
         id: 1,
         name: `Ship speed upgrade`,
         Description: `+0.2 ship speed`,
-        price: 3e3,
+        price: 0, // 3e3,
         rarity: 1,
         attributes: {
             movementSpeed: `20`
@@ -23,7 +23,7 @@ let itemTypes = [
         id: 2,
         name: `Cannon distance upgrade`,
         Description: `+5 cannon distance`,
-        price: 4e3,
+        price: 0, // 4e3,
         rarity: 1,
         attributes: {
             attackDistance: `5`
@@ -33,7 +33,7 @@ let itemTypes = [
         id: 3,
         name: `Damage upgrade`,
         Description: `+5 cannon damage`,
-        price: 5e3,
+        price: 0, // 5e3,
         rarity: 1,
         attributes: {
             attackDamage: `5`
@@ -43,7 +43,7 @@ let itemTypes = [
         id: 4,
         name: `Bruiser`,
         Description: `+5 cannon damage<br/>5 cannon fire rate`,
-        price: 3e4,
+        price: 0, // 3e4,
         rarity: 1,
         availableAt: [`Spain`, `Brazil`],
         attributes: {
@@ -55,7 +55,7 @@ let itemTypes = [
         id: 5,
         name: `Air Pegleg`,
         Description: `+1 ship speed`,
-        price: 22e3,
+        price: 0, // 22e3,
         rarity: 1,
         availableAt: [`Jamaica`],
         attributes: {
@@ -66,7 +66,7 @@ let itemTypes = [
         id: 6,
         name: `Reinforced Planks`,
         Description: `25% damage reduction`,
-        price: 35e3,
+        price: 0, // 35e3,
         rarity: 1,
         availableAt: [`Brazil`],
         attributes: {
@@ -77,7 +77,7 @@ let itemTypes = [
         id: 7,
         name: `Steel Barrel`,
         Description: `+30 cannon distance`,
-        price: 35e3,
+        price: 0, // 35e3,
         rarity: 1,
         availableAt: [`Labrador`],
         attributes: {
@@ -88,7 +88,7 @@ let itemTypes = [
         id: 8,
         name: `Sinker's Gloves`,
         Description: `+25 cannon fire rate`,
-        price: 45e3,
+        price: 0, // 45e3,
         rarity: 1,
         availableAt: [`Spain`, `Brazil`],
         attributes: {
@@ -99,7 +99,7 @@ let itemTypes = [
         id: 9,
         name: `Steel Reinforced Planks`,
         Description: `40% damage reduction`,
-        price: 2e5,
+        price: 0, // 2e5,
         rarity: 1,
         availableAt: [`Taiwan`],
         attributes: {
@@ -110,7 +110,7 @@ let itemTypes = [
         id: 10,
         name: `Advanced Toolkit`,
         Description: `+5 Regen`,
-        price: 2e5,
+        price: 0, // 2e5,
         rarity: 1,
         availableAt: [`Malaysia`],
         attributes: {
@@ -121,7 +121,7 @@ let itemTypes = [
         id: 11,
         name: `Blue Gunpowder`,
         Description: `+25 cannon damage`,
-        price: 2e5,
+        price: 0, // 2e5,
         rarity: 1,
         availableAt: [`Jamaica`],
         attributes: {
@@ -132,7 +132,7 @@ let itemTypes = [
         id: 12,
         name: `Drifter`,
         Description: `+10 cannon damage<br/>+1.5 ship speed`,
-        price: 2e5,
+        price: 0, // 2e5,
         rarity: 1,
         availableAt: [`Guinea`, `Labrador`],
         attributes: {
@@ -144,7 +144,7 @@ let itemTypes = [
         id: 13,
         name: `Nitro Peg`,
         Description: `+2 ship speed`,
-        price: 2e5,
+        price: 0, // 2e5,
         rarity: 1,
         availableAt: [`Jamaica`],
         attributes: {
@@ -155,7 +155,7 @@ let itemTypes = [
         id: 14,
         name: `Titanium Barrel`,
         Description: `+40 cannon distance`,
-        price: 25e4,
+        price: 0, // 25e4,
         rarity: 1,
         availableAt: [`Taiwan`],
         attributes: {
@@ -166,7 +166,7 @@ let itemTypes = [
         id: 15,
         name: `Demolisher`,
         Description: `+10 cannon damage<br/>+35 cannon fire rate<br/>Requirements:<br/> - Sink 10 ships<br/> - Trade goods worth 100,000 gold`,
-        price: 35e4,
+        price: 0, // 35e4,
         rarity: 1,
         availableAt: [`Jamaica`],
         attributes: {
@@ -178,7 +178,7 @@ let itemTypes = [
         id: 16,
         name: `Fountain of Youth`,
         Description: `Reset your skill points and allow them to be reallocated.<br/>Can only be bought once.`,
-        price: 15e4,
+        price: 0, // 15e4,
         rarity: 1,
         availableAt: [`Jamaica`]
     }
