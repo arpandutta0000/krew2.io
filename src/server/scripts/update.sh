03bbae68f3b947825452f14e8d14dcd84b1e0aa3
Damien Vesper <ldamienvesper@gmail.com>
2021-03-21 17:13:08 -0500

    Revert "fix invalid pass"
    This reverts commit c77776fab8b1b6967d65d7e37789405000990717.

--- src/server/scripts/update.sh
+++ src/server/scripts/update.sh
@@ -1,6 +1,6 @@
 cd /opt
 sudo rm -r krew2.io
-sudo git clone --depth=1 https://krewiogit:J5nETmjUkf59z9A@github.com/Krew-io/krew2.io.git
+sudo git clone --depth=1 https://krewiogit:J5nETmjUkf59z9A0@github.com/Krew-io/krew2.io.git
 cd /opt/krew2.io
 npm i
 npm run build
